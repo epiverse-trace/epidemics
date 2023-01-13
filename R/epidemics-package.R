ae418bce0d013b55139ec063b7b837d9c66ea4bd
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-01-13 16:26:19 +0000

    Infrastructure and convenience fun

--- R/epidemics-package.R
+++ R/epidemics-package.R
@@ -11,5 +11,7 @@
 #' @importFrom data.table .NGRP
 #' @importFrom data.table .SD
 #' @importFrom data.table data.table
+#' @importFrom Rcpp sourceCpp
+#' @useDynLib epidemics, .registration = TRUE
 ## usethis namespace: end
 NULL
---
+++ R/make_parameters.R
@@ -0,0 +1,16 @@
+
+#' Make convenient parameters
+#'
+#' @description Function to make convenient parameters.
+#' @export
+make_parameters_sir_stochastic <- function() {
+    list(
+       beta = 1e-1,
+       gamma = 0.05,
+       N = 1000,
+       S0 = 999,
+       I0 = 1,
+       R0 = 0,
+       tf = 200
+    )
+}

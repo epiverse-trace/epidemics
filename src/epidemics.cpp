df613ef1a50fabb7e1d87c58c16116d2a1787262
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-01-16 15:19:02 +0000

    Better formatting for epidemics.cpp

--- src/epidemics.cpp
+++ src/epidemics.cpp
@@ -1,22 +1,22 @@
+// Copyright 2023 'epidemics' authors. See repository licence in LICENSE.md.
 #include <Rcpp.h>
 #include <sir_stochastic.h>
 
 // [[Rcpp::interfaces(r, cpp)]]
 
-//' @title Stochastic SIR epidemic simulation
-//' @description Some description.
+//' @title Stochastic, continuous time SIR epidemic simulation
+//' @description An initial implementation of an SIR epidemic simulation.
 //' @param option A string giving the simulation option.
 //' @param parameters A named list of parameters to the simulation.
 //' @export
 // [[Rcpp::export]]
-Rcpp::DataFrame epi_demic(const Rcpp::List &parameters, const Rcpp::String &option = "sir_stochastic") {
-    if (option == "sir_stochastic") {
-        return sir_stochastic(
-            parameters["beta"], parameters["gamma"],
-            parameters["N"], parameters["S0"], parameters["I0"],
-            parameters["R0"], parameters["tf"]
-        );
-    } else {
-        Rcpp::stop("Option not found, option must be 'sir_stochastic'");
-    }
+Rcpp::DataFrame epi_demic(const Rcpp::List &parameters,
+                          const Rcpp::String &option = "sir_stochastic") {
+  if (option == "sir_stochastic") {
+    return sir_stochastic(parameters["beta"], parameters["gamma"],
+                          parameters["N"], parameters["S0"], parameters["I0"],
+                          parameters["R0"], parameters["tf"]);
+  } else {
+    Rcpp::stop("Option not found, option must be 'sir_stochastic'");
+  }
 }

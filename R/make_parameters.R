67b9ebc2866552f8f76696ed299253f12a882820
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-01-16 15:47:14 +0000

    Style edits

--- R/make_parameters.R
+++ R/make_parameters.R
@@ -4,13 +4,13 @@
 #' @description Function to make convenient parameters.
 #' @export
 make_parameters_sir_stochastic <- function() {
-    list(
-       beta = 1e-1,
-       gamma = 0.05,
-       N = 1000,
-       S0 = 999,
-       I0 = 1,
-       R0 = 0,
-       tf = 200
-    )
+  list(
+    beta = 1e-1,
+    gamma = 0.05,
+    N = 1000,
+    S0 = 999,
+    I0 = 1,
+    R0 = 0,
+    tf = 200
+  )
 }
--- tests/testthat/test-sir_stochastic.R
+++ tests/testthat/test-sir_stochastic.R
@@ -10,20 +10,20 @@ test_that("epi_demic runs withour errors", {
 
 # check statistical correctness
 test_that("epi_demic runs withour errors", {
-  outcome = epi_demic(
+  outcome <- epi_demic(
     parameters = make_parameters_sir_stochastic(),
     option = "sir_stochastic"
   )
-  
+
   expect_lte(
     max(outcome$value), 1.0
   )
   expect_gte(
     min(outcome$value), 0.0
   )
-  
+
   # check that all elements sum to 1.0 at the final timepoint
-  max_time = outcome[outcome$time == max(outcome$time),]
+  max_time <- outcome[outcome$time == max(outcome$time), ]
   expect_identical(
     sum(max_time$value), 1.0,
     tolerance = 1e-6

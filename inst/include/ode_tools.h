44286852c956192ae52c332a22d01f1264f26fcc
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-03-22 12:19:57 +0000

    SIR model and observer, package header

---
+++ inst/include/ode_tools.h
@@ -0,0 +1,42 @@
+// Copyright 2023 'epidemics' authors. See repository licence in LICENSE.md.
+#ifndef INST_INCLUDE_ODE_TOOLS_H_
+#define INST_INCLUDE_ODE_TOOLS_H_
+
+// [[Rcpp::plugins(cpp14)]]
+// [[Rcpp::depends(BH)]]
+
+// clang-format off
+#include <Rcpp.h>
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+#include <boost/numeric/odeint.hpp>
+// clang-format on
+
+// add to namespace ode
+namespace odetools {
+
+//[ rhs_function
+/* The type of container used to hold the state vector */
+typedef std::vector<double> state_type;
+
+//[ integrate_observer
+struct observer {
+  std::vector<state_type> &m_states;
+  std::vector<double> &m_times;
+
+  observer(std::vector<state_type> &states, std::vector<double> &times)
+      : m_states(states), m_times(times) {}
+
+  void operator()(const state_type &x, double t) {
+    m_states.push_back(x);
+    m_times.push_back(t);
+  }
+};
+//]
+
+}  // namespace odetools
+
+#endif  // INST_INCLUDE_ODE_TOOLS_H_

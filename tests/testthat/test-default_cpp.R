12e8c43939204684c3a84c00f4d090504d57321f
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-03-24 14:40:00 +0000

    Update test for epidemic_cpp

--- tests/testthat/test-default_cpp.R
+++ tests/testthat/test-default_cpp.R
@@ -1,53 +1,83 @@
 # Basic tests to check for functionality
 # Prepare some initial objects
-init <- rbind(
-  c(0.98, 0.01, 0.01, 0.0),
-  c(0.9, 0.09, 0.01, 0.0),
-  c(0.95, 0.04, 0.01, 0.0)
-)
-contact_matrix <- matrix(1.0, nrow = 3, ncol = 3)
-contact_matrix <- contact_matrix / rowSums(contact_matrix)
-diag(contact_matrix) <- rep(1.0, nrow(contact_matrix))
-
-p <- population(
-  contact_matrix = contact_matrix,
-  demography_vector = rep(10, 3),
-  initial_conditions = init
+population <- population(
+  name = "UK population",
+  contact_matrix = matrix(1),
+  demography_vector = 67e6,
+  initial_conditions = matrix(
+    c(0.9999, 0.0001, 0, 0),
+    nrow = 1, ncol = 4
+  )
 )
 
-test_that("Basic tests for default epidemic model", {
-  # create initial conditions
+# Prepare epi parameters
+r0 <- rep(1.5, nrow(population$contact_matrix))
+preinfectious_period <- rep(3, nrow(population$contact_matrix))
+infectious_period <- rep(7, nrow(population$contact_matrix))
 
-  data <- epidemic_default_cpp(
-    population = p,
-    beta = 0.5,
-    alpha = 0.1,
-    gamma = 0.05,
+test_that("Output of default epidemic model", {
+  # run model on data
+  data <- epidemic_cpp(
+    population = population,
+    r0 = r0,
+    preinfectious_period = preinfectious_period,
+    infectious_period = infectious_period,
     time_end = 10, increment = 1.0
   )
-  expect_type(data, "list")
-  expect_named(
-    data, c("x", "time")
+
+  # check for output type and contents
+  expect_s3_class(data, "data.table")
+  expect_length(data, ncol(population$initial_conditions) + 1L) # comp and time
+
+  # check for all positive values within the range 0 and total population size
+  expect_true(
+    all(
+      vapply(subset(data, select = -time), FUN = function(x) {
+        all(x >= 0 & x <= sum(population$demography_vector))
+      },
+      FUN.VALUE = TRUE
+      )
+    )
   )
-  # for an SEIR model
-  expect_length(
-    data[["x"]][[1]],
-    length(init)
+
+  # check for identical numbers of individuals at start and end
+  # Note only valid for models without births and deaths
+  expect_identical(
+    sum(subset(data[1, ], select = -time)),
+    sum(subset(tail(data, 1L), select = -time)),
+    tolerance = 1e-6
   )
 })
 
-test_that("Basic tests for data collection helper", {
-  data <- epidemic_default_cpp(
-    population = p,
-    beta = 0.5,
-    alpha = 0.1,
-    gamma = 0.05,
-    time_end = 200, increment = 1.0
+test_that("Larger R0 leads to larger final size in default epidemic model", {
+  # prepare epidemic model runs with different R0 estimates
+  r0_list <- list(
+    r0_low = r0,
+    r0_high = r0 + 1.0
   )
-  data <- output_to_df(data)
-  expect_named(
-    data,
-    c("time", do.call(paste0, expand.grid(c("S", "E", "I", "R"), seq(3)))),
-    ignore.order = TRUE
+
+  # get data
+  data <- lapply(
+    r0_list,
+    function(r0_) {
+      # run model on data
+      data <- epidemic_cpp(
+        population = population,
+        r0 = r0_,
+        preinfectious_period = preinfectious_period,
+        infectious_period = infectious_period,
+        time_end = 10, increment = 1.0
+      )
+    }
+  )
+
+  # get final size as total recoveries
+  final_sizes <- lapply(data, tail, 1)
+  final_sizes <- vapply(final_sizes, `[[`, FUN.VALUE = 1, "recovered1")
+
+  # test for effect of R0
+  expect_gt(
+    final_sizes["r0_high"],
+    final_sizes["r0_low"]
   )
 })

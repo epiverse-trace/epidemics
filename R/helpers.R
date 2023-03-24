b32806b77f19e924133f50d2c3d95b79acd6b494
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-03-24 14:39:33 +0000

    Update fn output_to_df

--- R/helpers.R
+++ R/helpers.R
@@ -1,11 +1,14 @@
-#' @title Convert [epidemic_default_cpp()] output to a data.frame
+#' @title Convert epidemic model output to a data.frame
 #'
-#' @param l A list, the output of a call to [epidemic_default_cpp()].
+#' @param l A list, usually the output of a call to [.epidemic_default_cpp()].
 #' @param compartments A character vector of the compartment names.
 #' @return A data.frame with N+1 columns, where N is the number of compartments,
 #' with the additional column being "time".
 #' @export
-output_to_df <- function(l, compartments = c("S", "E", "I", "R")) {
+output_to_df <- function(l, compartments = c(
+                           "susceptible", "exposed",
+                           "infectious", "recovered"
+                         )) {
   # input checking
   stopifnot(
     "Error: `l` must be a list" =
@@ -17,14 +20,16 @@ output_to_df <- function(l, compartments = c("S", "E", "I", "R")) {
   n_groups <- nrow(l[["x"]][[1]])
 
   # data are in the form S_1->N, E_1->N, etc.
-  data <- as.data.frame(
+  data <- data.table::as.data.table(
     matrix(
       unlist(l[["x"]]),
       ncol = n_col,
       byrow = TRUE
     )
   )
-  colnames(data) <- do.call(paste0, expand.grid(compartments, seq(n_groups)))
+  colnames(data) <- do.call(
+    paste0, expand.grid(compartments, seq(n_groups))
+  )
   data$time <- l[["time"]]
 
   # return data

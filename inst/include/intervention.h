a0ad2c517773c0c225ddddfa6ece98d9334a25ae
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-03-28 10:57:11 +0100

    Add intervention-contact matrix operations

---
+++ inst/include/intervention.h
@@ -0,0 +1,52 @@
+// Copyright 2023 'epidemics' authors. See repository licence in LICENSE.md.
+#ifndef INST_INCLUDE_INTERVENTION_H_
+#define INST_INCLUDE_INTERVENTION_H_
+
+// [[Rcpp::plugins(cpp14)]]
+// [[Rcpp::depends(BH)]]
+// [[Rcpp::depends(RcppEigen)]]
+
+// clang-format off
+#include <Rcpp.h>
+#include <RcppEigen.h>
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+// clang-format on
+
+// add to namespace ode
+namespace intervention {
+
+inline Eigen::MatrixXd intervention_on_cm(const Eigen::MatrixXd &cm,
+                                          const Rcpp::List &intervention) {
+  // create Eigen 1D array from R matrix passed in an list (class intervention)
+  Eigen::ArrayXd contact_reduction(
+      Rcpp::as<Eigen::ArrayXd>(intervention["contact_reduction"]));
+
+  // check inputs here
+  if (contact_reduction.size() != cm.rows()) {
+    Rcpp::stop(
+        "Error: Intervention must have the same number of elements as rows in "
+        "the contact matrix (demo. groups.)\n");
+  }
+
+  // modify the contact matrix as cm_mod = cm * (1 - intervention)
+  // for a percentage reduction in contacts
+  Eigen::MatrixXd modified_cm =
+      cm.array().colwise() * (1.0 - contact_reduction);
+  // transpose for rowwise array multiplication, as Eigen is col-major
+  return modified_cm;
+}
+
+inline bool is_intervention_active(const double &t,
+                                   const Rcpp::List &intervention) {
+  const double time_begin = intervention["time_begin"];
+  const double time_end = intervention["time_end"];
+  // input checking on intervention
+  return (t >= time_begin && t <= time_end);
+}
+
+}  // namespace intervention
+
+#endif  // INST_INCLUDE_INTERVENTION_H_

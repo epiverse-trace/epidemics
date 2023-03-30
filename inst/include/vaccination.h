d0a34815b25718cf4976cd646f4e2e4e4241e3d9
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-03-30 11:01:36 +0100

    Vaccination class helpers C++

--- inst/include/vaccination.h
+++ inst/include/vaccination.h
@@ -15,19 +15,35 @@
 #include <vector>
 // clang-format on
 
-// add to namespace intervention
+// add to namespace vaccination
 namespace vaccination {
 
+/// @brief Get the group specific vaccination rate from a `vaccination` object
+/// @param vaccination A `vaccination` object in the form of an Rcpp List
+/// @return An Eigen Array of group-specific vaccination rates.
+inline Eigen::ArrayXd get_nu(const Rcpp::List &vaccination) {
+  return Rcpp::as<Eigen::ArrayXd>(vaccination["nu"]);
+}
+
+/// @brief
+/// @param nu
+/// @param vaccination
+/// @param t
+/// @return
 inline Eigen::ArrayXd current_nu(const Eigen::ArrayXd &nu,
-                                 const Eigen::ArrayXd &t_vax_begin,
-                                 const Eigen::ArrayXd &t_vax_end,
+                                 const Rcpp::List &vaccination,
                                  const double &t) {
+  // create Eigen 1D array from R matrix passed in an list (class intervention)
+  Eigen::ArrayXd time_begin(
+      Rcpp::as<Eigen::ArrayXd>(vaccination["time_begin"]));
+  Eigen::ArrayXd time_end(Rcpp::as<Eigen::ArrayXd>(vaccination["time_end"]));
+
   Eigen::ArrayXd nu_active(nu.size());
   nu_active.fill(0.0);
 
   for (size_t i = 0; i < nu.size(); i++) {
     nu_active[i] =
-        (t >= t_vax_begin[i] && t <= t_vax_end[i]) ? nu[i] : nu_active[i];
+        (t >= time_begin[i] && t <= time_end[i]) ? nu[i] : nu_active[i];
   }
 
   return nu_active;

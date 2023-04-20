581d0c235c3b59836e7d5075883e8db9b55e9ca9
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-04-20 15:01:21 +0100

    Update output_to_df() fn, return long format

--- R/helpers.R
+++ R/helpers.R
@@ -1,36 +1,63 @@
-#' @title Convert epidemic model output to a data.frame
-#'
-#' @param l A list, usually the output of a call to [.epidemic_default_cpp()].
-#' @param compartments A character vector of the compartment names.
-#' @return A `data.frame` with N+1 columns, where N is the number of
-#' compartments, with the additional column being "time".
-output_to_df <- function(l, compartments = c(
-                           "susceptible", "exposed",
-                           "infectious", "recovered",
-                           "vaccinated"
-                         )) {
+#' Return ODE model output as a data.table
+#' @param output The model output, which must be a two element list (for
+#' epidemic) models, with the names "x" and "time", where "x" represents the
+#' condition of each compartment at each timestep in "time".
+#' @param model_arguments A list containing the model arguments passed to
+#' [epidemic()]. This is scanned for information on the population passed to
+#' the model, which must be a `population` object; see [population()].
+#' The `population` object is used to generate the names of the demographic
+#' groups, if these are named.
+#' @param compartments A vector for the model compartment names.
+#' @keywords internal
+#' @return A `data.table` with the columns "compartment", "demography_group",
+#' "value", and "time"; these specify the epidemiological compartment, the
+#' name of the demography group, the number of individuals of that group in the
+#' compartment, and the model timestep, respectively.
+#' Names for the demographic groups are generated if no names are provided in
+#' the `population` object; these are of the form "demo_group_X".
+output_to_df <- function(output, model_arguments, compartments) {
   # input checking
-  stopifnot(
-    "Error: `l` must be a list" =
-      is.list(l),
-    "Error: `l` must have names 'x' and 'time'" =
-      all(names(l) %in% c("x", "time"))
+  checkmate::assert_list(output,
+    any.missing = FALSE, all.missing = FALSE,
+    len = 2L, names = "unique"
   )
-  n_col <- length(l[["x"]][[1]])
-  n_groups <- nrow(l[["x"]][[1]])
+  checkmate::assert_names(
+    names(output),
+    must.include = c("x", "time")
+  )
+  checkmate::assert_character(compartments,
+    any.missing = FALSE,
+    all.missing = FALSE, unique = TRUE
+  )
 
-  # data are in the form S_1->N, E_1->N, etc.
-  data <- as.data.frame(
-    matrix(
-      unlist(l[["x"]]),
-      ncol = n_col,
-      byrow = TRUE
-    )
+  if ("population" %in% names(model_arguments)) {
+    names_demo_groups <- rownames(model_arguments$population$contact_matrix)
+    if (is.null(names_demo_groups)) {
+      names_demo_groups <- sprintf(
+        "demo_group_%i",
+        seq_len(nrow(model_arguments$population$contact_matrix))
+      )
+    }
+  }
+
+  # count groups and timesteps to generate vectors of compartment and demo
+  # group names
+  n_groups <- length(names_demo_groups)
+  n_timesteps <- length(output[["time"]])
+
+  vec_compartments <- rep(compartments, each = n_groups)
+  vec_compartments <- rep(vec_compartments, times = n_timesteps)
+
+  vec_demo_groups <- rep(names_demo_groups, length(compartments) * n_timesteps)
+
+  # return a data.table
+  data.table::data.table(
+    compartment = vec_compartments,
+    demography_group = vec_demo_groups,
+    value = unlist(output$x),
+    time = rep(output$time, each = n_groups * length(compartments))
   )
-  colnames(data) <- sprintf(
-    "%s_%i",
-    rep(compartments, each = n_groups),
-    rep(seq_len(n_groups), times = length(compartments))
+}
   )
   data$time <- l[["time"]]
 

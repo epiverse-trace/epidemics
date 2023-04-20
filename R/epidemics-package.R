0a4e128120cb05f43d9bfa9875108956ec4da7d3
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-04-20 17:17:35 +0100

    Use data.table, pkg infrastructure

--- R/epidemics-package.R
+++ R/epidemics-package.R
@@ -3,6 +3,15 @@
 
 ## usethis namespace: start
 #' @import RcppEigen
+#' @importFrom data.table :=
+#' @importFrom data.table .BY
+#' @importFrom data.table .EACHI
+#' @importFrom data.table .GRP
+#' @importFrom data.table .I
+#' @importFrom data.table .N
+#' @importFrom data.table .NGRP
+#' @importFrom data.table .SD
+#' @importFrom data.table data.table
 #' @importFrom Rcpp sourceCpp
 #' @useDynLib epidemics, .registration = TRUE
 ## usethis namespace: end

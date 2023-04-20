cbc1e46d8544f7d9c7ee61802a065c698cce4ebe
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-04-20 15:03:10 +0100

    Edit fns to read model compartments from library

--- R/read_from_library.R
+++ R/read_from_library.R
@@ -6,7 +6,7 @@
 #' outbreak models.
 #' @param model_name The model name. The only option currently available is
 #' "default", for the default epidemic compartmental SEIR-V model.
-#' @param which_function A string for the function requested, which may be one
+#' @param what A string for the function requested, which may be one
 #' of "model_function", "model_args_checker", and "model_args_prepper". These
 #' correspond to functions that run the model, check its arguments, and prepare
 #' the arguments for the model function, respectively.
@@ -14,12 +14,12 @@
 #' @keywords internal
 read_from_library <- function(model_type = "epidemic",
                               model_name = "default",
-                              which_function = "model_function") {
+                              what = "model_function") {
 
   # input checking
   checkmate::assert_string(model_type)
   checkmate::assert_string(model_name)
-  checkmate::assert_string(which_function)
+  checkmate::assert_string(what)
 
   # read model library
   model_library <- model_library()
@@ -28,17 +28,19 @@ read_from_library <- function(model_type = "epidemic",
   stopifnot(
     "Model library has no models of the specified type" =
       model_type %in% unique(model_library$model_type),
-    "Model library has no functions of the specified type" =
-      which_function %in% grep(
-        "function|args", colnames(model_library),
+    "Model library has no data of the specified type" =
+      what %in% grep(
+        "function|args|compartment", colnames(model_library),
         value = TRUE
       )
   )
 
-  fn <- model_library[
-    model_library$model_type == model_type &
-      model_library$model_name == model_name,
-  ][[which_function]]
+  fn <- unlist(
+    model_library[
+      model_library$model_type == model_type &
+        model_library$model_name == model_name,
+    ][[what]]
+  )
 
   if (length(fn) == 0L) {
     stop(

c6ad61b8e623b2a8ac9dc91b69898069207aa481
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-06-05 08:09:15 +0100

    Use `[[` accessor over `$`

--- R/check_args_default.R
+++ R/check_args_default.R
@@ -10,7 +10,7 @@
 
   # input checking on the infection object
   assert_infection(
-    mod_args$infection,
+    mod_args[["infection"]],
     extra_parameters = "preinfectious_period"
   )
 
@@ -19,26 +19,30 @@
     model_name = "default", what = "compartments"
   )
   assert_population(
-    mod_args$population,
+    mod_args[["population"]],
     compartments = compartments_default
   )
 
   # add null intervention and vaccination if these are missing
   # if not missing, check that they conform to expectations
   if (!"intervention" %in% names(mod_args)) {
-    mod_args$intervention <- no_intervention(
-      mod_args$population
+    mod_args[["intervention"]] <- no_intervention(
+      mod_args[["population"]]
     )
   } else {
-    assert_intervention(mod_args$intervention, mod_args$population)
+    assert_intervention(mod_args[["intervention"]], mod_args[["population"]])
   }
   if (!"vaccination" %in% names(mod_args)) {
-    mod_args$vaccination <- no_vaccination(
-      mod_args$population
+    mod_args[["vaccination"]] <- no_vaccination(
+      mod_args[["population"]]
     )
   } else {
     # default model only supports a single dose vaccination
-    assert_vaccination(mod_args$vaccination, doses = 1L, mod_args$population)
+    assert_vaccination(
+      mod_args[["vaccination"]],
+      doses = 1L,
+      mod_args[["population"]]
+    )
   }
 
   # return arguments invisibly
@@ -50,28 +54,29 @@
 .prepare_args_epidemic_default <- function(mod_args) {
   # prepare the contact matrix and the initial conditions
   # scale the contact matrix by the maximum real eigenvalue
-  mod_args$population$contact_matrix <-
-    mod_args$population$contact_matrix /
-      max(Re(eigen(mod_args$population$contact_matrix)$values))
+  mod_args[["population"]][["contact_matrix"]] <-
+    mod_args[["population"]][["contact_matrix"]] /
+      max(Re(eigen(mod_args[["population"]][["contact_matrix"]])$values))
 
   # scale rows of the contact matrix by the corresponding group population
-  mod_args$population$contact_matrix <-
-    mod_args$population$contact_matrix /
-      mod_args$population$demography_vector
+  mod_args[["population"]][["contact_matrix"]] <-
+    mod_args[["population"]][["contact_matrix"]] /
+      mod_args[["population"]][["demography_vector"]]
 
   # prepare initial conditions by scaling with demography
-  mod_args$population$initial_conditions <-
-    mod_args$population$initial_conditions *
-      mod_args$population$demography_vector
+  mod_args[["population"]][["initial_conditions"]] <-
+    mod_args[["population"]][["initial_conditions"]] *
+      mod_args[["population"]][["demography_vector"]]
 
   # calculate beta and gamma
-  mod_args$gamma <- 1.0 / mod_args$infection$infectious_period
-  mod_args$alpha <- 1.0 / mod_args$infection$preinfectious_period
-  mod_args$beta <- mod_args$infection$r0 / mod_args$infection$infectious_period
+  mod_args[["gamma"]] <- 1.0 / mod_args[["infection"]][["infectious_period"]]
+  mod_args[["alpha"]] <- 1.0 / mod_args[["infection"]][["preinfectious_period"]]
+  mod_args[["beta"]] <- mod_args[["infection"]][["r0"]] /
+    mod_args[["infection"]][["infectious_period"]]
   # nu is passed through vaccination class
 
   # remove infection object, as parameters are passed as alpha, beta, gamma
-  mod_args$infection <- NULL
+  mod_args[["infection"]] <- NULL
 
   return(mod_args)
 }
--- R/check_args_vacamole.R
+++ R/check_args_vacamole.R
@@ -11,7 +11,7 @@
 
   # input checking on infection object
   assert_infection(
-    mod_args$infection,
+    mod_args[["infection"]],
     extra_parameters = c(
       "preinfectious_period", "eta", "omega",
       "susc_reduction_vax", "hosp_reduction_vax", "mort_reduction_vax"
@@ -24,26 +24,26 @@
   )
   # input checking on population parameters
   assert_population(
-    mod_args$population,
+    mod_args[["population"]],
     compartments = compartments_vacamole
   )
 
   # input checking on vaccination parameters
   # the Vacamole model expects two vaccination doses
   assert_vaccination(
-    mod_args$vaccination,
+    mod_args[["vaccination"]],
     doses = 2L,
-    population = mod_args$population
+    population = mod_args[["population"]]
   )
 
   # add null intervention if this is missing
   # if not missing, check that it conforms to expectations
   if (!"intervention" %in% names(mod_args)) {
-    mod_args$intervention <- no_intervention(
-      mod_args$population
+    mod_args[["intervention"]] <- no_intervention(
+      mod_args[["population"]]
     )
   } else {
-    assert_intervention(mod_args$intervention, mod_args$population)
+    assert_intervention(mod_args[["intervention"]], mod_args[["population"]])
   }
 
   # return arguments invisibly
@@ -55,42 +55,43 @@
 .prepare_args_epidemic_vacamole <- function(mod_args) {
   # prepare the contact matrix and the initial conditions
   # scale the contact matrix by the maximum real eigenvalue
-  mod_args$population$contact_matrix <-
-    mod_args$population$contact_matrix /
-      max(Re(eigen(mod_args$population$contact_matrix)$value))
+  mod_args[["population"]][["contact_matrix"]] <-
+    mod_args[["population"]][["contact_matrix"]] /
+      max(Re(eigen(mod_args[["population"]][["contact_matrix"]])$value))
 
   # scale rows of the contact matrix by the corresponding group population
-  mod_args$population$contact_matrix <-
-    mod_args$population$contact_matrix /
-      mod_args$population$demography_vector
+  mod_args[["population"]][["contact_matrix"]] <-
+    mod_args[["population"]][["contact_matrix"]] /
+      mod_args[["population"]][["demography_vector"]]
 
   # prepare initial conditions by scaling with demography
-  mod_args$population$initial_conditions <-
-    mod_args$population$initial_conditions *
-      mod_args$population$demography_vector
+  mod_args[["population"]][["initial_conditions"]] <-
+    mod_args[["population"]][["initial_conditions"]] *
+      mod_args[["population"]][["demography_vector"]]
 
-  # calculate infection parameters
-  mod_args$gamma <- 1.0 / mod_args$infection$infectious_period
-  mod_args$alpha <- 1.0 / mod_args$infection$preinfectious_period
-  mod_args$beta <- mod_args$infection$r0 / mod_args$infection$infectious_period
+  # calculate infection parametersß
+  mod_args[["gamma"]] <- 1.0 / mod_args[["infection"]][["infectious_period"]]
+  mod_args[["alpha"]] <- 1.0 / mod_args[["infection"]][["preinfectious_period"]]
+  mod_args[["beta"]] <- mod_args[["infection"]][["r0"]] /
+    mod_args[["infection"]][["infectious_period"]]
 
-  mod_args$eta <- mod_args$infection$eta
-  mod_args$omega <- mod_args$infection$omega
+  mod_args[["eta"]] <- mod_args[["infection"]][["eta"]]
+  mod_args[["omega"]] <- mod_args[["infection"]][["omega"]]
 
   # modified parameters for the two-dose vaccinated compartment
-  mod_args$beta_v <- mod_args$beta *
-    (1.0 - mod_args$infection$susc_reduction_vax)
+  mod_args[["beta_v"]] <- mod_args[["beta"]] *
+    (1.0 - mod_args[["infection"]][["susc_reduction_vax"]])
 
-  mod_args$eta_v <- mod_args$infection$eta *
-    (1.0 - mod_args$infection$hosp_reduction_vax)
+  mod_args[["eta_v"]] <- mod_args[["infection"]][["eta"]] *
+    (1.0 - mod_args[["infection"]][["hosp_reduction_vax"]])
 
-  mod_args$omega_v <- mod_args$infection$omega *
-    (1.0 - mod_args$infection$hosp_reduction_vax)
+  mod_args[["omega_v"]] <- mod_args[["infection"]][["omega"]] *
+    (1.0 - mod_args[["infection"]][["hosp_reduction_vax"]])
 
   # nu is passed through vaccination class
 
   # remove infection object, as parameters are passed as alpha, beta, gamma
-  mod_args$infection <- NULL
+  mod_args[["infection"]] <- NULL
 
   return(mod_args)
 }

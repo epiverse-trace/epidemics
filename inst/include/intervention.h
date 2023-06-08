1b3b2d84f02ad8e9b913d6ed43c43f8fccec08f5
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-06-08 16:43:07 +0100

    Intervention.h: Add fn cumulative_intervention()

--- inst/include/intervention.h
+++ inst/include/intervention.h
@@ -14,7 +14,30 @@
 // add to namespace ode
 namespace intervention {
 
-inline Eigen::MatrixXd intervention_on_cm(const Eigen::MatrixXd &cm,
+inline Eigen::ArrayXd cumulative_intervention(const double &t,
+                                              const Rcpp::List &intervention) {
+  // copy the list elements
+  // TODO(all): reduce copying
+  Rcpp::NumericVector time_begin = intervention["time_begin"];
+  Rcpp::NumericVector time_end = intervention["time_end"];
+  Rcpp::NumericMatrix cr = intervention["contact_reduction"];
+
+  Rcpp::NumericVector eff_con_red(cr.size());
+
+  // iterate over the Rcpp vector time_begin, a member of the list-like
+  // intervention-class
+  for (size_t i = 0; i < time_begin.size(); i++) {
+    if (t >= time_begin(i) && t <= time_end(i)) {
+      eff_con_red += cr(Rcpp::_, i);
+    }
+  }
+
+  Eigen::ArrayXd effective_contact_reduction(
+      Rcpp::as<Eigen::ArrayXd>(eff_con_red));
+
+  return effective_contact_reduction;
+}
+
                                           const Rcpp::List &intervention) {
   // create Eigen 1D array from R matrix passed in an list (class intervention)
   Eigen::ArrayXd contact_reduction(

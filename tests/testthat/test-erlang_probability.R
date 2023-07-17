7b112541c850fd36f70e9c160f3526596a4a036f
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-07-17 16:16:32 +0100

    Test equivalence between R and C++ Erlang fns

--- tests/testthat/test-erlang_probability.R
+++ tests/testthat/test-erlang_probability.R
@@ -8,3 +8,21 @@ test_that("Computing Erlang probabilites", {
 
   expect_snapshot(v)
 })
+
+test_that("Computing Erlang probabilites with C++", {
+
+  # using R only
+  v <- .prob_discrete_erlang(5L, 1)
+
+  expect_vector(v, ptype = numeric())
+  expect_length(v, 12L) ## this is a known value and hardcoded
+
+  expect_snapshot(v)
+})
+
+test_that("Expect R-C++ equivalence", {
+  expect_identical(
+    .prob_discrete_erlang(5L, 1),
+    compute_erlang_discrete_prob(5, 1)
+  )
+})

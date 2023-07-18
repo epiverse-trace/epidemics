707f6ba7240dd01e848df578420dba4299f79d5c
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-07-18 17:53:03 +0100

    Helper fns for pop_size and initial conditions C++

--- inst/include/population.h
+++ inst/include/population.h
@@ -23,6 +23,24 @@ inline Eigen::MatrixXd get_contact_matrix(const Rcpp::List &population) {
   return Rcpp::as<Eigen::MatrixXd>(population["contact_matrix"]);
 }
 
+/// @brief Get the total population size
+/// @param population A `population` class object, handled as an Rcpp::List.
+/// @return An integer value for the total population size.
+inline int get_population_size(const Rcpp::List &population) {
+  Rcpp::NumericVector demography_vector = population["demography_vector"];
+  return static_cast<int>(Rcpp::sum(demography_vector));
+}
+
+/// @brief Get the population initial conditions
+/// @param population A `population` class object, handled as an Rcpp::List.
+/// @return An Rcpp::NumericMatrix of the initial conditions per age group. See
+/// the documentation in R for the `population` class.
+inline Rcpp::NumericMatrix get_initial_conditions(
+    const Rcpp::List &population) {
+  Rcpp::NumericMatrix initial_conditions = population["initial_conditions"];
+  return initial_conditions;
+}
+
 }  // namespace population
 
 #endif  // INST_INCLUDE_POPULATION_H_

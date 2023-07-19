92aabf40b6203f1af4d7818830706f18c4a30055
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-07-19 16:34:39 +0100

    Documentation for the ebola model src file

--- src/epidemic_ebola.cpp
+++ src/epidemic_ebola.cpp
@@ -5,6 +5,11 @@
 
 //' @title Run an SEIR model with Erlang passage times
 //'
+//' @description Wrapper function for an SEIR compartmental model with Erlang
+//' passage times based on Getz and Dougherty (2017) J. Biological Dynamics,
+//' and developed to model the West African ebola virus disease outbreak of 2014
+//' .
+//'
 //' @param population An object of the `population` class, which holds a
 //' population contact matrix, a demography vector, and the initial conditions
 //' of each demographic group. See [population()].
@@ -19,16 +24,18 @@
 //' distribution of passage times through the infectious compartment.
 //' @param time_end A single integer for the maximum simulation time; this is
 //' assumed to be in days.
-//' @return An integer matrix with as many rows as the number of timesteps
-//' (given by `time_end`), and four columns, one for each compartment, SEIR,
-//' in that order.
-//' @export
+//' @return A list with two elements, `x`, and integer matrix with as many rows
+//' as the number of timesteps (given by `time_end`), and four columns, one for
+//' each compartment, susceptible, exposed, infectious, recovered, in that order
+//' ; and `times`, a vector of the simulation times, taken to be days.
+//' This output is intended to be passed to [output_to_df()] to be converted
+//' into a data.frame for further analysis.
+//' @keywords internal
 // [[Rcpp::export(name=".epidemic_ebola_cpp")]]
-Rcpp::IntegerMatrix epidemic_ebola_cpp(const Rcpp::List population,
-                                       const double &beta, const int &shape_E,
-                                       const double &rate_E, const int &shape_I,
-                                       const double &rate_I,
-                                       const int &time_end) {
+Rcpp::List epidemic_ebola_cpp(const Rcpp::List population, const double &beta,
+                              const int &shape_E, const double &rate_E,
+                              const int &shape_I, const double &rate_I,
+                              const int &time_end) {
   return epidemics::epidemic_ebola(beta, shape_E, rate_E, shape_I, rate_I,
                                    time_end, population);
 }

98509af809648869ff46bd458b4e660a8c24c740
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-08-14 14:49:36 +0100

    Update tests for erlang prob funs

--- tests/testthat/test-erlang_probability.R
+++ tests/testthat/test-erlang_probability.R
@@ -1,6 +1,6 @@
 test_that("Computing Erlang probabilites", {
   # using R only
-  v <- compute_erlang_discrete_prob(5, 1)
+  v <- prob_discrete_erlang(5, 1)
 
   expect_vector(v, ptype = numeric())
   expect_length(v, 12L) ## this is a known value and hardcoded
@@ -10,7 +10,7 @@ test_that("Computing Erlang probabilites", {
 
 test_that("Computing Erlang probabilites with C++", {
   # using R only
-  v <- .prob_discrete_erlang(5L, 1)
+  v <- prob_discrete_erlang_cpp(5L, 1)
 
   expect_vector(v, ptype = numeric())
   expect_length(v, 12L) ## this is a known value and hardcoded
@@ -20,7 +20,7 @@ test_that("Computing Erlang probabilites with C++", {
 
 test_that("Expect R-C++ equivalence", {
   expect_identical(
-    .prob_discrete_erlang(5L, 1),
-    compute_erlang_discrete_prob(5, 1)
+    prob_discrete_erlang_cpp(5L, 1),
+    prob_discrete_erlang(5, 1)
   )
 })

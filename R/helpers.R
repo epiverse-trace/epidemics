162358a04ab32dac3070a86a968195f16c555b1e
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-08-17 12:23:48 +0100

    Add parameter accessor function

--- R/helpers.R
+++ R/helpers.R
@@ -1,3 +1,34 @@
+
+#' Get parameters from \{epidemics\} classes
+#'
+#' @param x An object of one of the classes provided by _epidemics_:
+#' `<population>`, `<infection>`, `<intervention>`, or `<vaccination>`.
+#' @param parameter A string for the parameter to access from `x`.
+#'
+#' @return An object of the class of `parameter` from `x`.
+#' @export
+#'
+#' @examples
+#' # prepare an infection and access the r0 member
+#' pandemic <- infection(
+#'   r0 = 1.5,
+#'   preinfectious_period = 3,
+#'   infectious_period = 7
+#' )
+#'
+#' get_parameter(pandemic, "r0")
+get_parameter <- function(x, parameter) {
+  stopifnot(
+    "`x` must be <population>, <infection>, <intervention>, or <vaccination>" =
+      is_population(x) || is_infection(x) || is_intervention(x) ||
+        is_vaccination(x)
+  )
+  checkmate::assert_string(parameter)
+
+  # return list element
+  x[[parameter]]
+}
+
 #' Return ODE model output as a data.table
 #' @param output The model output, which must be a two element list (for
 #' epidemic) models, with the names "x" and "time", where "x" represents the

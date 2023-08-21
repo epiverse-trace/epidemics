846142a2b5f20e6903263378bbd983c75523d22e
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-08-21 14:58:39 +0100

    Remove infection param fn, superseded by translate_intervention

--- inst/include/infection.h
+++ inst/include/infection.h
@@ -4,28 +4,13 @@
 
 // clang-format off
 #include <Rcpp.h>
-#include <map>
+#include <unordered_map>
+#include <string>
 // clang-format on
 
 /// @brief A namespace for helpers related to the infection class
 namespace infection {
 
-/// @brief Convert an `Rcpp::List` of infection parameters into a `std::map`
-/// @param infection_parameters
-/// @return
-inline std::map<std::string, double> make_infection_param_map(
-    Rcpp::List &infection_parameters) {
-  std::map<std::string, double> result;
-
-  Rcpp::CharacterVector keys = infection_parameters.names();
-  Rcpp::NumericVector values(infection_parameters);
-
-  for (int i = 0; i < infection_parameters.size(); ++i) {
-    result[std::string(keys[i])] = values[i];
-  }
-
-  return result;
-}
 
 }  // namespace infection
 

4f73d1a503b4ca83f8970c58fc7d949720f87416
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-08-23 14:48:16 +0100

    Update vacamole model header with rate interventions

--- inst/include/epidemic_vacamole.h
+++ inst/include/epidemic_vacamole.h
@@ -29,7 +29,9 @@ namespace epidemics {
 
 /// @brief Struct containing the Vacamole epidemic ODE system
 struct epidemic_vacamole {
-  const double beta, beta_v, alpha, omega, omega_v, eta, eta_v, gamma;
+  // two maps for the infection parameters, one for dynamic modification
+  const std::unordered_map<std::string, double> infection_params;
+  std::unordered_map<std::string, double> infection_params_temp;
   const Eigen::MatrixXd contact_matrix;
   Eigen::MatrixXd cm_temp;
   // related to interventions
@@ -38,17 +40,22 @@ struct epidemic_vacamole {
   // related to vaccination
   const Eigen::MatrixXd vax_time_begin, vax_time_end, vax_nu;
   Eigen::MatrixXd vax_nu_current;
+  const std::unordered_map<std::string, intervention::rate_intervention>
+      interventions;
   // npi, interv, pop
 
   /// @brief Constructor for the Vacamole epidemic struct
-  /// @param beta The transmission rate for un-or-single vaccinated individuals
-  /// @param beta_v The transmission rate for double-vaccinated individuals
-  /// @param alpha The rate at which individuals become infectious
-  /// @param omega The mortality rate of un-or-single-vaccinated individuals
-  /// @param omega_v The mortality rate of double-vaccinated individuals
-  /// @param eta The hospitalisation rate of un-or-single-vaccinated individuals
-  /// @param eta_v The hospitalisation rate of double-vaccinated individuals
-  /// @param gamma The recovery rate
+  /// @param infection_params An unordered map of string-double pairs, with the
+  /// infection parameters as keys, and parameter values as values. The
+  /// model parameters are:
+  /// - beta The transmission rate for un-or-single vaccinated individuals
+  /// - beta_v The transmission rate for double-vaccinated individuals
+  /// - alpha The rate at which individuals become infectious
+  /// - omega The mortality rate of un-or-single-vaccinated individuals
+  /// - omega_v The mortality rate of double-vaccinated individuals
+  /// - eta The hospitalisation rate of un-or-single-vaccinated individuals
+  /// - eta_v The hospitalisation rate of double-vaccinated individuals
+  /// - gamma The recovery rate
   /// @param contact_matrix The population contact matrix
   /// @param npi_time_begin The intervention start times
   /// @param npi_time_end The intervention end times
@@ -56,24 +63,20 @@ struct epidemic_vacamole {
   /// @param vax_time_begin The age- and dose-specific vaccination start time
   /// @param vax_time_end The age- and dose-specific vaccination end time
   /// @param vax_nu The age- and dose-specific vaccination rate
-  epidemic_vacamole(const double beta, const double beta_v, const double alpha,
-                    const double omega, const double omega_v, const double eta,
-                    const double eta_v, const double gamma,
-                    const Eigen::MatrixXd contact_matrix,
-                    const Rcpp::NumericVector npi_time_begin,
-                    const Rcpp::NumericVector npi_time_end,
-                    const Rcpp::NumericMatrix npi_cr,
-                    const Eigen::MatrixXd vax_time_begin,
-                    const Eigen::MatrixXd vax_time_end,
-                    const Eigen::MatrixXd vax_nu)
-      : beta(beta),
-        beta_v(beta_v),
-        alpha(alpha),
-        omega(omega),
-        omega_v(omega_v),
-        eta(eta),
-        eta_v(eta_v),
-        gamma(gamma),
+  /// @param interventions An unordered map of string-intervention pairs. The
+  /// keys must refer to parameters in `infection_params`. The `intervention`
+  /// struct is defined in `inst/include/intervention.h`,
+  epidemic_vacamole(
+      const std::unordered_map<std::string, double>& infection_params,
+      const Eigen::MatrixXd contact_matrix,
+      const Rcpp::NumericVector npi_time_begin,
+      const Rcpp::NumericVector npi_time_end, const Rcpp::NumericMatrix npi_cr,
+      const Eigen::MatrixXd vax_time_begin, const Eigen::MatrixXd vax_time_end,
+      const Eigen::MatrixXd vax_nu,
+      const std::unordered_map<std::string, intervention::rate_intervention>&
+          interventions)
+      : infection_params(infection_params),
+        infection_params_temp(infection_params),
         contact_matrix(contact_matrix),
         cm_temp(contact_matrix),
         npi_time_begin(npi_time_begin),
@@ -82,7 +85,8 @@ struct epidemic_vacamole {
         vax_time_begin(vax_time_begin),
         vax_time_end(vax_time_end),
         vax_nu(vax_nu),
-        vax_nu_current(vax_nu) {}
+        vax_nu_current(vax_nu),
+        interventions(interventions) {}
 
   /// @brief Operator for the default model
   /// @param x The initial state of the population - rows represent age groups
@@ -100,6 +104,10 @@ struct epidemic_vacamole {
     cm_temp = intervention::intervention_on_cm(
         t, contact_matrix, npi_time_begin, npi_time_end, npi_cr);
 
+    // rate interventions
+    infection_params_temp =
+        intervention::intervention_on_rates(t, infection_params, interventions);
+
     // get current vaccination rate
     vax_nu_current =
         vaccination::current_nu(t, vax_nu, vax_time_begin, vax_time_end);
@@ -112,8 +120,8 @@ struct epidemic_vacamole {
 
     // compartmental transitions without accounting for contacts
     // Susceptible to exposed
-    Eigen::ArrayXd sToE =
-        beta * x.col(0).array() * (cm_temp * (x.col(5) + x.col(6))).array();
+    Eigen::ArrayXd sToE = infection_params_temp["beta"] * x.col(0).array() *
+                          (cm_temp * (x.col(5) + x.col(6))).array();
 
     // Susceptible to vaccinated with one dose
     Eigen::ArrayXd sToV1 = vax_nu_current.col(0).array() * x.col(0).array();
@@ -121,41 +129,41 @@ struct epidemic_vacamole {
     Eigen::ArrayXd v1ToV2 = vax_nu_current.col(1).array() * x.col(1).array();
 
     // Vaccinated one dose to exposed - same as susceptible to exposed
-    Eigen::ArrayXd v1ToE =
-        beta * x.col(1).array() * (cm_temp * (x.col(5) + x.col(6))).array();
+    Eigen::ArrayXd v1ToE = infection_params_temp["beta"] * x.col(1).array() *
+                           (cm_temp * (x.col(5) + x.col(6))).array();
     // Vaccinated two doses to exposed - uses different beta
-    Eigen::ArrayXd v2ToEv =
-        beta_v * x.col(2).array() * (cm_temp * (x.col(5) + x.col(6))).array();
+    Eigen::ArrayXd v2ToEv = infection_params_temp["beta_v"] * x.col(2).array() *
+                            (cm_temp * (x.col(5) + x.col(6))).array();
 
     // Exposed unvaccinated or not protected to infectious
-    Eigen::ArrayXd eToI = alpha * x.col(3).array();
+    Eigen::ArrayXd eToI = infection_params_temp["alpha"] * x.col(3).array();
     // Exposed vaccinated to infectious
-    Eigen::ArrayXd evToIv = alpha * x.col(4).array();
+    Eigen::ArrayXd evToIv = infection_params_temp["alpha"] * x.col(4).array();
 
     // Infectious to hospitalised
-    Eigen::ArrayXd iToH = eta * x.col(5).array();
+    Eigen::ArrayXd iToH = infection_params_temp["eta"] * x.col(5).array();
     // Vaccinated infectious to hospitalised
-    Eigen::ArrayXd ivToHv = eta_v * x.col(6).array();
+    Eigen::ArrayXd ivToHv = infection_params_temp["eta_v"] * x.col(6).array();
 
     // Infectious to dead
-    Eigen::ArrayXd iToD = omega * x.col(5).array();
+    Eigen::ArrayXd iToD = infection_params_temp["omega"] * x.col(5).array();
     // Infectious vaccinated to dead
-    Eigen::ArrayXd ivToD = omega_v * x.col(6).array();
+    Eigen::ArrayXd ivToD = infection_params_temp["omega_v"] * x.col(6).array();
 
     // Hospitalised to dead
-    Eigen::ArrayXd hToD = omega * x.col(7).array();
+    Eigen::ArrayXd hToD = infection_params_temp["omega"] * x.col(7).array();
     // Hospitalised vaccinated to dead
-    Eigen::ArrayXd hvToD = omega_v * x.col(8).array();
+    Eigen::ArrayXd hvToD = infection_params_temp["omega_v"] * x.col(8).array();
 
     // Infectious to recovered
-    Eigen::ArrayXd iToR = gamma * x.col(5).array();
+    Eigen::ArrayXd iToR = infection_params_temp["gamma"] * x.col(5).array();
     // Infectious vaccinated to recovered
-    Eigen::ArrayXd ivToR = gamma * x.col(6).array();
+    Eigen::ArrayXd ivToR = infection_params_temp["gamma"] * x.col(6).array();
 
     // Hospitalised to recovered
-    Eigen::ArrayXd hToR = gamma * x.col(7).array();
+    Eigen::ArrayXd hToR = infection_params_temp["gamma"] * x.col(7).array();
     // Hospitalised vaccinated to recovered
-    Eigen::ArrayXd hvToR = gamma * x.col(8).array();
+    Eigen::ArrayXd hvToR = infection_params_temp["gamma"] * x.col(8).array();
 
     // compartmental changes accounting for contacts
     dxdt.col(0) = -sToE - sToV1;           // -β*S*contacts*(I+Iv) - ν1*S

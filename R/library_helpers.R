53c969f563c4a8788a5e490b48a4996fcf324dcf
bahadzie <bahadzie@gmail.com>
2023-10-09 18:37:22 +0100

    Remove get_model_names()

--- R/library_helpers.R
+++ R/library_helpers.R
@@ -12,19 +12,3 @@ model_library <- function() {
     simplifyVector = TRUE
   )
 }
-
-#' Get names of available models
-#'
-#' @param model_type The model type. The only option currently available is
-#' "epidemic". Future development may see the addition of more model types,
-#' including endemic disease compartmental models, and branching process
-#' outbreak models.
-#'
-#' @export
-get_model_names <- function(model_type = "epidemic") {
-  checkmate::assert_string(model_type)
-  model_type <- match.arg(arg = model_type, several.ok = FALSE)
-  # read in the model library using the predefined helper
-  model_lib <- model_library()
-  model_lib[model_lib$model_type == model_type, ]$model_name
-}

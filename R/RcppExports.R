ce29e1bda2fabb1b4971d7db564f0d8520a7fd34
bahadzie <bahadzie@gmail.com>
2023-10-26 20:20:43 +0000

    Response to GH Actions findings

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -41,8 +41,16 @@
 #' as specified in the initial conditions matrix (see [population()]).
 #' The second list element is a vector of timesteps.
 #' @keywords internal
-.model_default_cpp <- function(initial_state, beta, alpha, gamma, contact_matrix, npi_time_begin, npi_time_end, npi_cr, vax_time_begin, vax_time_end, vax_nu, rate_interventions, time_dependence, time_end = 100.0, increment = 1.0) {
-    .Call(`_epidemics_model_default_cpp_internal`, initial_state, beta, alpha, gamma, contact_matrix, npi_time_begin, npi_time_end, npi_cr, vax_time_begin, vax_time_end, vax_nu, rate_interventions, time_dependence, time_end, increment)
+.model_default_cpp <- function(
+    initial_state, beta, alpha, gamma, contact_matrix, npi_time_begin,
+    npi_time_end, npi_cr, vax_time_begin, vax_time_end, vax_nu,
+    rate_interventions, time_dependence, time_end = 100.0, increment = 1.0) {
+    .Call(
+        `_epidemics_model_default_cpp_internal`, initial_state, beta, alpha,
+        gamma, contact_matrix, npi_time_begin, npi_time_end, npi_cr,
+        vax_time_begin, vax_time_end, vax_nu, rate_interventions,
+        time_dependence, time_end, increment
+    )
 }
 
 #' @title Run an SEIR model with Erlang passage times
@@ -68,13 +76,19 @@
 #' assumed to be in days.
 #' @return A list with two elements, `x`, and integer matrix with as many rows
 #' as the number of timesteps (given by `time_end`), and four columns, one for
+#' https://lintr.r-lib.org/reference/object_usage_linter.html
 #' each compartment, susceptible, exposed, infectious, recovered, in that order
 #' ; and `times`, a vector of the simulation times, taken to be days.
 #' This output is intended to be passed to [output_to_df()] to be converted
 #' into a data.frame for further analysis.
 #' @keywords internal
-.model_ebola_cpp <- function(initial_state, population_size, beta, shape_E, rate_E, shape_I, rate_I, time_end) {
-    .Call(`_epidemics_model_ebola_cpp_internal`, initial_state, population_size, beta, shape_E, rate_E, shape_I, rate_I, time_end)
+.model_ebola_cpp <- function(
+    initial_state, population_size, beta, shape_E, rate_E,
+    shape_I, rate_I, time_end) {
+    .Call(
+        `_epidemics_model_ebola_cpp_internal`, initial_state,
+        population_size, beta, shape_E, rate_E, shape_I, rate_I, time_end
+    )
 }
 
 #' @title Run the RIVM Vacamole model
@@ -132,8 +146,17 @@
 #' as specified in the initial conditions matrix (see [population()]).
 #' The second list element is a vector of timesteps.
 #' @keywords internal
-.model_vacamole_cpp <- function(initial_state, beta, beta_v, alpha, omega, omega_v, eta, eta_v, gamma, contact_matrix, npi_time_begin, npi_time_end, npi_cr, vax_time_begin, vax_time_end, vax_nu, rate_interventions, time_dependence, time_end = 100.0, increment = 1.0) {
-    .Call(`_epidemics_model_vacamole_cpp_internal`, initial_state, beta, beta_v, alpha, omega, omega_v, eta, eta_v, gamma, contact_matrix, npi_time_begin, npi_time_end, npi_cr, vax_time_begin, vax_time_end, vax_nu, rate_interventions, time_dependence, time_end, increment)
+.model_vacamole_cpp <- function(
+    initial_state, beta, beta_v, alpha, omega, omega_v, eta, eta_v, gamma,
+    contact_matrix, npi_time_begin, npi_time_end, npi_cr, vax_time_begin,
+    vax_time_end, vax_nu, rate_interventions, time_dependence,
+    time_end = 100.0, increment = 1.0) {
+    .Call(
+        `_epidemics_model_vacamole_cpp_internal`, initial_state, beta, beta_v,
+        alpha, omega, omega_v, eta, eta_v, gamma, contact_matrix,
+        npi_time_begin, npi_time_end, npi_cr, vax_time_begin, vax_time_end,
+        vax_nu, rate_interventions, time_dependence, time_end, increment
+    )
 }
 
 #' @title Compute the discrete probability of the truncated Erlang distribution
@@ -143,4 +166,3 @@
 prob_discrete_erlang_cpp <- function(shape, rate) {
     .Call(`_epidemics_prob_discrete_erlang_cpp`, shape, rate)
 }
-

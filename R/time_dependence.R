e07dc3d16dc12c59ea68f4c5f18b27d8f9a5ec26
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-11-08 17:05:15 +0000

    Default no time dependence uses renamed rate

--- R/time_dependence.R
+++ R/time_dependence.R
@@ -1,12 +1,13 @@
 #' Specify no time dependence of model rates
 #'
-#' @return A list with a single named element, `beta`, which is a function
-#' that returns its second argument. This matches the specification for
+#' @return A list with a single named element, `transmissibility`, which is a
+#' function that returns its second argument. This matches the specification for
 #' functions to be passed as the `time_dependence` argument of epidemic
 #' functions.
-#' @export
+#' @keywords internal
 no_time_dependence <- function() {
+  # TODO: make internal
   list(
-    beta = function(time, x) x
+    transmissibility = function(time, x) x
   )
 }

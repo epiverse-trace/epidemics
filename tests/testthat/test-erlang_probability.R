746d7f3c1d8121ed9e864ab51e3d588a324b3a8c
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-11-09 16:30:50 +0000

    Remove test for prob_erlang_cpp

--- tests/testthat/test-erlang_probability.R
+++ tests/testthat/test-erlang_probability.R
@@ -7,20 +7,3 @@ test_that("Computing Erlang probabilites", {
 
   expect_snapshot(v)
 })
-
-test_that("Computing Erlang probabilites with C++", {
-  # using R only
-  v <- prob_discrete_erlang_cpp(5L, 1)
-
-  expect_vector(v, ptype = numeric())
-  expect_length(v, 12L) ## this is a known value and hardcoded
-
-  expect_snapshot(v)
-})
-
-test_that("Expect R-C++ equivalence", {
-  expect_identical(
-    prob_discrete_erlang_cpp(5L, 1),
-    prob_discrete_erlang(5, 1)
-  )
-})

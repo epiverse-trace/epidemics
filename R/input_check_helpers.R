32fad1dce46c9c47da7685ffdffa12b88229741c
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-11-22 16:28:49 +0000

    Update input check helpers - assert_*()

--- R/input_check_helpers.R
+++ R/input_check_helpers.R
@@ -6,24 +6,26 @@
 #' to check the population parameters required by each model. This
 #' function is for internal use in argument checking functions.
 #'
-#' @param x A [population] object.
+#' @param x A `<population>` object.
 #' @param compartments A character vector giving the names of model compartments
 #' whose length is taken as the reference for the number of columns in the
 #' `initial_conditions` matrix in `x`.
+#' @param demography_vector An optional numeric vector whose length is used to
+#' check the length of the demography vector present in `x`.
 #'
 #' @keywords internal
 #'
-#' @return Silently returns the `population` object `x`.
+#' @return Silently returns the `<population>` object `x`.
 #' Primarily called for its side effects of throwing errors when `x` does not
 #' meet certain requirements.
-assert_population <- function(x, demography_groups = NULL, compartments) {
+assert_population <- function(x, compartments, demography_vector = NULL) {
   # check for input class
   checkmate::assert_class(x, "population")
 
   # check that population has a set number of demography groups
   checkmate::assert_numeric(
     get_parameter(x, "demography_vector"),
-    len = demography_groups # checked against NULL in most models
+    len = demography_vector # checked against NULL in most models
   )
 
   # check that population has as many compartments in initial conditions
@@ -31,7 +33,7 @@ assert_population <- function(x, demography_groups = NULL, compartments) {
   checkmate::assert_matrix(
     get_parameter(x, "initial_conditions"),
     mode = "numeric", # this is also checked when initialising a population
-    nrows = demography_groups, # this is NULL for most models
+    nrows = demography_vector, # this is NULL for most models
     ncols = length(compartments)
   )
 
@@ -49,16 +51,16 @@ assert_population <- function(x, demography_groups = NULL, compartments) {
 #'
 #' @param x A [vaccination] object.
 #' @param doses The number of doses expected in the vaccination object.
-#' @param population An optional argument which is a [population] object.
+#' @param population An optional argument which is a `<population>` object.
 #' When present, this is used to check whether the vaccination object `x` has
 #' corresponding values for each demographic group in `population`.
 #'
 #' @keywords internal
 #'
-#' @return Silently returns the `vaccination` object `x`.
+#' @return Silently returns the `<vaccination>` object `x`.
 #' Primarily called for its side effects of throwing errors when `x` does not
 #' meet certain requirements.
-assert_vaccination <- function(x, doses, population) {
+assert_vaccination <- function(x, doses, population = NULL) {
   # check for input class
   checkmate::assert_class(x, "vaccination")
   checkmate::assert_number(doses, finite = TRUE, lower = 1L)
@@ -73,7 +75,7 @@ assert_vaccination <- function(x, doses, population) {
 
   # if a population is provided, check that the rows of `nu`
   # match the number of demography groups
-  if (!missing(population)) {
+  if (!is.null(population)) {
     checkmate::assert_class(population, "population")
     checkmate::assert_matrix(
       get_parameter(x, "nu"),
@@ -103,38 +105,38 @@ assert_vaccination <- function(x, doses, population) {
 #'
 #' @keywords internal
 #'
-#' @return Silently returns the `intervention` object `x`.
+#' @return Silently returns the `<intervention>`-superclass object `x` of the
+#' same class as `x`, i.e., `<rate_intervention>` or `<contacts_intervention>`.
 #' Primarily called for its side effects of throwing errors when `x` does not
 #' meet certain requirements.
 assert_intervention <- function(x, type = c("contacts", "rate"),
-                                population) {
+                                population = NULL) {
   # check for input class
   type <- match.arg(type, several.ok = FALSE)
   switch(type,
-    contacts = checkmate::assert_class(x, "contacts_intervention"),
-    rate = checkmate::assert_class(x, "rate_intervention")
-  )
-
-  if (type == "contacts") {
-    # check the population
-    # check that the length of reduction is the same as the number
-    # of demographic groups
-    n_demo_groups <- NULL
-    if (!missing(population)) {
-      n_demo_groups <- length(get_parameter(population, "demography_vector"))
+    contacts = {
+      checkmate::assert_class(x, "contacts_intervention")
+      # check that the length of reduction is the same as the number
+      # of demographic groups
+      n_demo_groups <- NULL
+      if (!is.null(population)) {
+        n_demo_groups <- length(get_parameter(population, "demography_vector"))
+      }
+      checkmate::assert_matrix(
+        get_parameter(x, "reduction"),
+        mode = "numeric",
+        nrows = n_demo_groups
+      )
+    },
+    rate = {
+      checkmate::assert_class(x, "rate_intervention")
+      checkmate::assert_numeric(
+        get_parameter(x, "reduction"),
+        lower = 0.0, upper = 1.0,
+        len = length(get_parameter(x, "time_begin"))
+      )
     }
-    checkmate::assert_matrix(
-      get_parameter(x, "reduction"),
-      mode = "numeric",
-      nrows = n_demo_groups
-    )
-  } else if (type == "rate") {
-    checkmate::assert_numeric(
-      get_parameter(x, "reduction"),
-      lower = 0.0, upper = 1.0,
-      len = length(get_parameter(x, "time_begin"))
-    )
-  }
+  )
 
   # invisibly return x
   invisible(x)

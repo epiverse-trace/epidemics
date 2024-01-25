abb126c5efbb43c60a725dac1b846c9c09750679
pratikunterwegs <pratikgupte16@gmail.com>
2024-01-25 12:41:20 +0000

    Add `population_change` Cpp struct

--- inst/include/population.h
+++ inst/include/population.h
@@ -3,13 +3,11 @@
 #ifndef INST_INCLUDE_POPULATION_H_
 #define INST_INCLUDE_POPULATION_H_
 
-// [[Rcpp::plugins(cpp14)]]
-// [[Rcpp::depends(BH)]]
-// [[Rcpp::depends(RcppEigen)]]
-
 // clang-format off
 #include <Rcpp.h>
 #include <RcppEigen.h>
+
+#include <vector>
 // clang-format on
 
 // add to namespace population
@@ -40,6 +38,33 @@ inline Rcpp::NumericMatrix get_initial_conditions(
   Rcpp::NumericMatrix initial_conditions = population["initial_conditions"];
   return initial_conditions;
 }
+
+/// @brief Hold parameters for changes to the model population.
+/// This is special functionality for the diphtheria model applied to
+/// humanitarian camps.
+struct population_change {
+  const std::vector<double> times;
+  const std::vector<Rcpp::NumericVector>
+      value;  // note this is a proportion of the total
+  const int n_demo_groups;
+
+  /// @brief Constructor for the population change struct
+  /// @param times The times at which the population changes. Changes apply to
+  /// the SUSCEPTIBLES compartment only; the assumption is that the camp is the
+  /// locus of the outbreak.
+  /// @param value The proportional change in the population size.
+  /// Each demographic group increases by the same proportion.
+  population_change(const std::vector<double> &times,
+                    const std::vector<Rcpp::NumericVector> &value)
+      : times(times), value(value), n_demo_groups(value[0].size()) {}
+
+  // member function for population change at time t
+  /// @brief Calculate the population change at time t
+  /// @param t The current timestep
+  /// @return An Eigen Array of population size changes.
+  const Eigen::ArrayXd get_population_change(const double &t);
+};
+
 
 }  // namespace population
 

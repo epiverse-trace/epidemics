3196dbfaac3352732d75eb40916a656fc13eaf48
pratikunterwegs <pratikgupte16@gmail.com>
2024-02-12 16:16:48 +0000

    Fns to cross check model elements

--- R/tools.R
+++ R/tools.R
@@ -67,3 +67,111 @@ test_recyclable <- function(x) {
 
   x
 }
+
+#' Prepare a `<population>` for an epidemic model
+#'
+#' @param x A `<population>`.
+#'
+#' @return A named list of "contact_matrix", the population social contacts
+#' matrix scaled by the largest real eigenvalue and the demography vector, and
+#' "initial_state", the proportional initial state multiplied by the demography
+#' vector.
+#' @keywords internal
+.prepare_population <- function(x) {
+  # no input checks on internal function
+
+  # prepare the contact matrix and the initial conditions
+  # scale the contact matrix by the maximum real eigenvalue
+  # scale rows of the contact matrix by the corresponding group population
+  contact_matrix <- x[["contact_matrix"]]
+  contact_matrix <- (contact_matrix / max(Re(eigen(contact_matrix)$values))) /
+    x[["demography_vector"]]
+
+  # prepare initial conditions by scaling with demography
+  initial_state <- x[["initial_conditions"]] * x[["demography_vector"]]
+
+  # return list
+  list(contact_matrix = contact_matrix, initial_state = initial_state)
+}
+
+#' Cross-check an intervention list
+#'
+#' @param x A named list of `<intervention>` objects.
+#' @param population A `<population>` to which any `<contact_intervention>` in
+#' `x` applies, and with which it must be compatible.
+#' @param allowed_targets A character vector of targets allowed for the
+#' `<intervention>`s in `x`. May contain "contacts", and the names of any
+#' infection parameters.
+#'
+#' @return A named list with at least the elements "contacts" describing a
+#' `<contacts_intervention>` on `population`, and a `<rate_intervention>` on
+#' the transmissibility parameter. If these are present in `x`, they are
+#' returned as is, or substituted if missing such that the function output is
+#' suitable for processing for a C++ model function. Any other interventions
+#' are also returned. If `x` is `NULL`, dummy
+#' contact and rate interventions are returned in a list.
+#' @keywords internal
+.cross_check_intervention <- function(x, population, allowed_targets) {
+  # create dummy intervention set
+  tmp_intervention <- list(
+    contacts = no_contacts_intervention(population),
+    transmissibility = no_rate_intervention()
+  )
+  if (is.null(x)) {
+    return(tmp_intervention)
+  }
+
+  # check that interventions are on allowed targets
+  # these are typically "contacts" and some infection parameters
+  checkmate::assert_names(names(x), subset.of = allowed_targets)
+
+  # check that contact interventions are suitable for population
+  if ("contacts" %in% names(x)) {
+    assert_intervention(x[["contacts"]], "contacts", population)
+  }
+
+  # replace dummy values with user values if avaialable, and return
+  tmp_intervention[names(x)] <- x
+  tmp_intervention
+}
+
+#' Cross-check a `<vaccination>`
+#'
+#' @param x A `<vaccination>`
+#' @param population A `<population>`
+#' @param doses A single number for the expected number of doses.
+#'
+#' @return Returns `x` after checking that it is suitable for `population`, or
+#' a dummy vaccination regime.
+#' @keywords internal
+.cross_check_vaccination <- function(x, population, doses) {
+  if (is.null(x)) {
+    no_vaccination(population)
+  } else {
+    assert_vaccination(x, doses = doses, population)
+    x
+  }
+}
+
+#' Cross-check time-dependence
+#'
+#' @param x A named list of functions that describe how an infection parameter
+#' changes with time.
+#' @param allowed_targets A character vector of targets for `x`; these are
+#' typically infection parameters.
+#'
+#' @return If `x` is not `NULL`, returns `x`; otherwise a dummy function is
+#' returned.
+#' @keywords internal
+.cross_check_timedep <- function(x, allowed_targets) {
+  if (is.null(x)) {
+    no_time_dependence()
+  } else {
+    checkmate::assert_names(names(x), subset.of = allowed_targets)
+    x
+  }
+}
+
+.cross_check_popchange <- function(x, population) {
+  x
+}

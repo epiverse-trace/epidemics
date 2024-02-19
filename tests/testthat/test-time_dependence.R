6f6a6c3420dcdc222dd6941ef47c79a8632cc95e
pratikunterwegs <pratikgupte16@gmail.com>
2024-02-19 13:40:43 +0000

    Rationalise tests for time-dependence

--- tests/testthat/test-time_dependence.R
+++ tests/testthat/test-time_dependence.R
@@ -1,81 +1,18 @@
 #### Tests for passing time-dependence functions ####
 
-# Basic tests fo no_time_dependence()
-test_that("Basic test for no_time_dependence", {
+# Basic tests fo .no_time_dependence()
+test_that("Time dependence: Basic expectations", {
   expect_identical(
-    no_time_dependence(),
+    .no_time_dependence(),
     list(transmissibility = function(time, x) x),
+    ignore_function_env = TRUE
+  )
+
+  expect_identical(
+    .no_time_dependence("recovery_rate"),
+    list(recovery_rate = function(time, x) x),
     ignore_function_env = TRUE
   )
 })
 
-# set up initial conditions
-polymod <- socialmixr::polymod
-contact_data <- socialmixr::contact_matrix(
-  polymod,
-  countries = "United Kingdom",
-  age.limits = c(0, 20, 40),
-  symmetric = TRUE
-)
-contact_matrix <- t(contact_data$matrix)
-demography_vector <- contact_data$demography$population
-
-# Prepare some initial objects
-uk_population <- population(
-  name = "UK population",
-  contact_matrix = contact_matrix,
-  demography_vector = demography_vector,
-  initial_conditions = matrix(
-    c(0.9999, 0, 0.0001, 0, 0),
-    nrow = nrow(contact_matrix), ncol = 5L,
-    byrow = TRUE
-  )
-)
-
-# prepare function
-mod_transmissibility <- function(time, x, tmax = 365 / 4) {
-  x + (x * sinpi(time / tmax))
-}
-
-#### Tests for functioning and equivalence ####
-test_that("Basic expectations for time dependence functions", {
-  expect_no_condition(
-    model_default_cpp(
-      population = uk_population,
-      time_dependence = list(
-        transmissibility = mod_transmissibility
-      ),
-      time_end = 365, increment = 1
-    )
-  )
-
-  expect_no_condition(
-    model_default_cpp(
-      population = uk_population,
-      time_dependence = list(
-        transmissibility = mod_transmissibility
-      ),
-      time_end = 365, increment = 1
-    )
-  )
-
-  data_cpp <- model_default_cpp(
-    population = uk_population,
-    time_dependence = list(
-      transmissibility = mod_transmissibility
-    ),
-    time_end = 365, increment = 1
-  )
-  data_r <- model_default_cpp(
-    population = uk_population,
-    time_dependence = list(
-      transmissibility = mod_transmissibility
-    ),
-    time_end = 365, increment = 1
-  )
-
-  expect_identical(
-    tail(data_r, 20),
-    tail(data_cpp, 20)
-  )
-})
+# NOTE: time-dependence functionality for models is tested in model test files

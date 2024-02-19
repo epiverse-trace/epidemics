fdbc4b882b1428d221e2327ae7fd460ae909c8ae
pratikunterwegs <pratikgupte16@gmail.com>
2024-02-19 15:54:23 +0000

    Update tests for new_infections()

---
+++ tests/testthat/_snaps/new_infections.md
@@ -0,0 +1,20 @@
+# New infections: aggregation over demographic group
+
+    Code
+      data_new_infections
+    Output
+      Key: <time, demography_group>
+            time demography_group new_infections
+           <num>           <char>          <num>
+        1:     0     demo_group_1         0.0000
+        2:     0     demo_group_2         0.0000
+        3:     1     demo_group_1       127.8277
+        4:     1     demo_group_2       127.8278
+        5:     2     demo_group_1       298.7246
+       ---                                      
+      198:    98     demo_group_2      9377.6294
+      199:    99     demo_group_1      9631.0889
+      200:    99     demo_group_2      9666.8141
+      201:   100     demo_group_1      9926.4184
+      202:   100     demo_group_2      9964.4648
+
--- tests/testthat/test-new_infections.R
+++ tests/testthat/test-new_infections.R
@@ -1,4 +1,4 @@
-# Tests for calculation of new infections
+#### Tests for calculation of new infections ####
 initial_conditions <- matrix(
   c(0.9999, 0.0001, 0, 0, 0), # note "infectious" are zero
   nrow = 1, ncol = 5L,
@@ -26,7 +26,7 @@ vaccination <- vaccination(
 )
 
 # run epidemic simulation with vaccination
-time_end <- 200
+time_end <- 100
 increment <- 1
 data <- model_default(
   population = uk_population,
@@ -35,19 +35,20 @@ data <- model_default(
   increment = increment
 )
 
-test_that("New infections are correctly caculated", {
+test_that("New infections: basic expectations and statistical correctness", {
+  # expect run with no conditions
+  expect_no_condition(
+    new_infections(data, compartments_from_susceptible = "vaccinated")
+  )
   data_ <- new_infections(data, compartments_from_susceptible = "vaccinated")
 
-  # expect correct number of rows - single demography group
+  # expect correct number of rows for two demography groups
   expect_identical(
-    data_$time,
-    seq(0, time_end, increment)
+    data_$time, seq(0, time_end, increment)
   )
-
   # expect columns with specific names
   expect_named(
-    data_,
-    c("time", "demography_group", "new_infections"),
+    data_, c("time", "demography_group", "new_infections"),
     ignore.order = TRUE
   )
 
@@ -69,7 +70,7 @@ test_that("New infections are correctly caculated", {
   )
 })
 
-test_that("New infections without accounting for vaccination", {
+test_that("New infections: statistical correctness without vaccination", {
   data_ <- new_infections(data)
   new_infections <- data_$new_infections
 
@@ -84,5 +85,63 @@ test_that("New infections without accounting for vaccination", {
   expect_identical(
     new_infections,
     delta_susc
+  )
+})
+
+#### Tests for multiple demographic groups ####
+# Prepare data with multiple demographic groups
+initial_conditions <- matrix(
+  c(0.9999, 0.0001, 0, 0, 0), # note "infectious" are zero
+  nrow = 1, ncol = 5L,
+  byrow = TRUE
+)
+initial_conditions <- rbind(initial_conditions, initial_conditions)
+colnames(initial_conditions) <- c(
+  "susceptible", "exposed", "infectious", "recovered", "vaccinated"
+)
+
+# create a dummy population
+demography_vector <- 67e6 * c(0.4, 0.6)
+uk_population <- population(
+  name = "UK population",
+  contact_matrix = matrix(1, 2, 2),
+  demography_vector = demography_vector,
+  initial_conditions = initial_conditions
+)
+
+# run epidemic simulation with vaccination
+time_end <- 100L
+data <- model_default(
+  population = uk_population,
+  time_end = time_end,
+  increment = increment
+)
+
+test_that("New infections: aggregation over demographic group", {
+  # expect that structure and contents for multiple groups is correct
+  data_new_infections <- new_infections(data)
+  expect_identical(
+    nrow(data_new_infections),
+    (time_end + 1L) * length(demography_vector)
+  )
+  # NOTE: use `expect_equal()` to avoid multiple integer-float conversions
+  expect_equal(
+    data_new_infections$time,
+    rep(seq(0, time_end), each = length(demography_vector))
+  )
+  # snapshot test
+  expect_snapshot(
+    data_new_infections
+  )
+
+  # expect that setting `by_group = FALSE` is identical to manual aggregation
+  data_new_infections_agg <- new_infections(data, by_group = FALSE)
+  data_infections_manual_agg <- aggregate(
+    data_new_infections, new_infections ~ time,
+    FUN = sum
+  )
+  expect_identical(
+    data_new_infections_agg$new_infections,
+    data_infections_manual_agg$new_infections
   )
 })

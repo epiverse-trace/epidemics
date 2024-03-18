962afd52cc5242626cc661cc4c3eb603184854e1
pratikunterwegs <pratikgupte16@gmail.com>
2024-03-18 13:20:38 +0000

    Add touchstone benchmarking script

--- touchstone/script.R
+++ touchstone/script.R
@@ -1,30 +1,79 @@
 # see `help(run_script, package = 'touchstone')` on how to run this
 # interactively
 
-# TODO OPTIONAL Add directories you want to be available in this file or during the
-# benchmarks.
+# TODO OPTIONAL Add directories you want to be available in this file or during
+# the benchmarks.
 # touchstone::pin_assets("some/dir")
 
 # installs branches to benchmark
 touchstone::branch_install()
 
-# benchmark a function call from your package (two calls per branch)
+#### Models without interventions ####
+# NOTE: all use default SCALAR parameters
+# Default model
 touchstone::benchmark_run(
-  # expr_before_benchmark = source("dir/data.R"), #<-- TODO OTPIONAL setup before benchmark
-  random_test = yourpkg::f(), #<- TODO put the call you want to benchmark here
-  n = 2
+  expr_before_benchmark = {
+    source("touchstone/setup.R")
+  },
+  default = {
+    model_default(
+      population = uk_population,
+      time_end = 600, increment = 1.0
+    )
+  },
+  n = 50
 )
 
-# TODO OPTIONAL benchmark any R expression (six calls per branch)
-# touchstone::benchmark_run(
-#   more = {
-#     if (TRUE) {
-#       y <- yourpkg::f2(x = 3)
-#     }
-#   }, #<- TODO put the call you want to benchmark here
-#   n = 6
-# )
+# Vacamole model
+touchstone::benchmark_run(
+  expr_before_benchmark = {
+    source("touchstone/setup.R")
+  },
+  default = {
+    model_vacamole(
+      population = uk_population,
+      time_end = 600, increment = 1.0
+    )
+  },
+  n = 50
+)
 
+# Diphtheria model
+touchstone::benchmark_run(
+  expr_before_benchmark = {
+    source("touchstone/setup.R")
+  },
+  default = {
+    model_diphtheria(
+      population = uk_population,
+      time_end = 600, increment = 1.0
+    )
+  },
+  n = 50
+)
+
+# Ebola model
+model_ebola(
+  population = guinea_population,
+  time_end = 100
+)
+touchstone::benchmark_run(
+  expr_before_benchmark = {
+    source("touchstone/setup.R")
+  },
+  default = {
+    withr::with_seed(
+      seed = 1,
+      {
+        model_ebola(
+          population = guinea_population,
+          time_end = 100
+        )
+      }
+    )
+  },
+  n = 50
+)
 
 # create artifacts used downstream in the GitHub Action
 touchstone::benchmark_analyze()

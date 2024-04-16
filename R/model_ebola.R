6aaed424b1dcb66844ce94885fb66b6f3e7a53a2
pratikunterwegs <pratikgupte16@gmail.com>
2024-04-16 11:41:39 +0100

    Use furrr to parallelise ebola replicates

--- R/model_ebola.R
+++ R/model_ebola.R
@@ -507,170 +507,174 @@ model_ebola <- function(population,
   # Use seed preservation to ensure that random number streams are preserved
   # for runs of intervention*parameter sets; i.e., run `i` of each set
   # should use the same random numbers
-  output_runs <- Map(seq_len(replicates), local_seeds, f = function(xi_, seed) {
-    # use local seed from pre-generated seeds
-    withr::local_seed(seed)
+  output_runs <- furrr::future_map(
+    .x = seq_len(replicates), .f = function(xi_) {
+      # use local seed from pre-generated seeds
+      # withr::local_seed(seed)
 
-    # NOTE: the original ebola model code continues here
-    # Prepare data matrix and assign initial state
-    # NOTE: returns time_end + 1 rows for consistency with ODE models
-    sim_data <- matrix(
-      NA_integer_,
-      nrow = time_end + 1, ncol = length(compartments)
-    )
-    colnames(sim_data) <- compartments
-    sim_data[1, ] <- initial_state # at time = 1
-
-    # Get a small random number for use with the hospitalisation functionality
-    # save function calls by reusing the vector of values
-    rounding_factor <- stats::rnorm(n_infectious_boxcars - 1, 0, 1e-2)
-
-    # Distribute individuals into sub-compartments
-    exposed_current <- as.vector(
-      stats::rmultinom(
-        1,
-        size = sim_data[1, "exposed"],
-        prob = exposed_boxcar_rates
+      # NOTE: the original ebola model code continues here
+      # Prepare data matrix and assign initial state
+      # NOTE: returns time_end + 1 rows for consistency with ODE models
+      sim_data <- matrix(
+        NA_integer_,
+        nrow = time_end + 1, ncol = length(compartments)
       )
-    )
-    exposed_past <- exposed_current
+      colnames(sim_data) <- compartments
+      sim_data[1, ] <- initial_state # at time = 1
 
-    infectious_current <- as.vector(
-      stats::rmultinom(1,
-        size = sim_data[1, "infectious"],
-        prob = infectious_boxcar_rates
-      )
-    )
-    infectious_past <- infectious_current
+      # Get a small random number for use with the hospitalisation functionality
+      # save function calls by reusing the vector of values
+      rounding_factor <- stats::rnorm(n_infectious_boxcars - 1, 0, 1e-2)
 
-    hospitalised_current <- as.vector(
-      stats::rmultinom(1,
-        size = sim_data[1, "hospitalised"],
-        prob = infectious_boxcar_rates
-      )
-    )
-    hospitalised_past <- hospitalised_current
-
-    # NOTE: "funeral" has no sub-compartments
-    funeral_trans_current <- sim_data[1, "funeral"]
-    funeral_trans_past <- funeral_trans_current
-
-    # Run the simulation from time t = 2 to t = time_end + 1 (nrows in data)
-    # A loop is required as conditions at each time t + 1 depend on time t.
-    for (time in seq(2, nrow(sim_data))) {
-      # make a copy to assign time-dependent and intervention-affected values
-      params <- parameters
-
-      # apply time dependence before interventions
-      time_dependent_params <- Map(
-        parameters[names(time_dependence)],
-        time_dependence,
-        f = function(x, func) {
-          func(time = time, x = x) # NOTE: time taken from loop index!
-        }
-      )
-      # assign time-modified param values
-      params[names(time_dependent_params)] <- time_dependent_params
-
-      # check if an intervention is active and apply it to rates
-      params <- .intervention_on_rates(
-        t = time, interventions = intervention, parameters = params
-      )
-
-      # transmission modifiers - 1.0 for baseline, user-provided for ETU risk
-      # and funeral risk
-      transmission_rate_modifiers <- c(
-        1.0, params[["etu_risk"]], params[["funeral_risk"]]
-      )
-
-      # get current transmission_rate as
-      # base rate * intervention * p(infectious)
-      # TODO: check if transmissibilities should be summed or averaged
-      current_transmission_rate <- sum(params[["transmission_rate"]] *
-        transmission_rate_modifiers *
-        sim_data[time - 1, c("infectious", "hospitalised", "funeral")]) /
-        population_size
-      exposure_prob <- 1.0 - exp(-current_transmission_rate)
-
-      # calculate new exposures
-      new_exposed <- stats::rbinom(
-        1, sim_data[time - 1, "susceptible"], exposure_prob
-      )
-      # handle non-zero new exposures
-      if (new_exposed > 0) {
-        # distribute new exposures and add past exposures moved forward by one
-        # timestep
-        exposed_current <- as.vector(
-          stats::rmultinom(1, size = new_exposed, prob = exposed_boxcar_rates)
-        ) +
-          c(exposed_past[-1], 0)
-      } else {
-        exposed_current <- c(exposed_past[-1], 0)
-      }
-
-      # handle hospitalisations first, as required for infectious compartment
-      # new hospitalisations are a proportion of individuals from infectious
-      # sub-compartments. Add a small normally distributed error to proportion
-      # hospitalised to facilitate rounding to avoid fractional individuals
-      # NOTE: proportion hospitalised = 1 - proportion community
-      hospitalised_current <- round(
-        # the SD of the normal distribution is small enough that values
-        # added to zero lead to rounding to zero
-        # first infectious_past compartment cannot be hospitalised and is
-        # transferred to funeral compartment
-        (infectious_past[-1] * (1.0 - params[["prop_community"]])) +
-          rounding_factor
-      )
-
-      # calculate new infectious individuals
-      new_infectious <- exposed_past[1]
-      # handle non-zero new infectious
-      if (new_infectious > 0) {
-        infectious_current <- as.vector(
-          stats::rmultinom(
-            1,
-            size = new_infectious, prob = infectious_boxcar_rates
-          ) +
-            c(infectious_past[-1] - hospitalised_current, 0)
+      # Distribute individuals into sub-compartments
+      exposed_current <- as.vector(
+        stats::rmultinom(
+          1,
+          size = sim_data[1, "exposed"],
+          prob = exposed_boxcar_rates
         )
-      } else {
-        infectious_current <- c(infectious_past[-1] - hospitalised_current, 0)
-      }
-
-      # continue handling hospitalisations
-      # concat zero to hospitalised_current at start as no infectious can go
-      # to this compartment
-      hospitalised_current <- c(0, hospitalised_current) +
-        c(hospitalised_past[-1], 0)
-
-      # calculate new individuals in the funeral transmission class
-      funeral_trans_current <- infectious_past[1]
-
-      # calculate new safely removed as the final hospitalised
-      # sub-compartments and new burials of potentially transmitting funerals
-      new_removed <- hospitalised_past[1] + funeral_trans_past
-
-      # set past vectors to current vectors
+      )
       exposed_past <- exposed_current
+
+      infectious_current <- as.vector(
+        stats::rmultinom(1,
+          size = sim_data[1, "infectious"],
+          prob = infectious_boxcar_rates
+        )
+      )
       infectious_past <- infectious_current
+
+      hospitalised_current <- as.vector(
+        stats::rmultinom(1,
+          size = sim_data[1, "hospitalised"],
+          prob = infectious_boxcar_rates
+        )
+      )
       hospitalised_past <- hospitalised_current
+
+      # NOTE: "funeral" has no sub-compartments
+      funeral_trans_current <- sim_data[1, "funeral"]
       funeral_trans_past <- funeral_trans_current
 
-      # prepare the data for output
-      sim_data[time, "susceptible"] <- sim_data[time - 1, "susceptible"] -
-        new_exposed
-      sim_data[time, "exposed"] <- sum(exposed_current)
-      sim_data[time, "infectious"] <- sum(infectious_current)
-      sim_data[time, "hospitalised"] <- sum(hospitalised_current)
-      sim_data[time, "funeral"] <- funeral_trans_current
-      sim_data[time, "removed"] <- sim_data[time - 1, "removed"] +
-        new_removed
-    }
+      # Run the simulation from time t = 2 to t = time_end + 1 (nrows in data)
+      # A loop is required as conditions at each time t + 1 depend on time t.
+      for (time in seq(2, nrow(sim_data))) {
+        # make a copy to assign time-dependent and intervention-affected values
+        params <- parameters
 
-    # return simulated data matrix and time as a two element list
-    # replicate id is handled in `.output_to_df_ebola()`
-    list(x = sim_data, time = seq(0, time_end))
-  })
+        # apply time dependence before interventions
+        time_dependent_params <- Map(
+          parameters[names(time_dependence)],
+          time_dependence,
+          f = function(x, func) {
+            func(time = time, x = x) # NOTE: time taken from loop index!
+          }
+        )
+        # assign time-modified param values
+        params[names(time_dependent_params)] <- time_dependent_params
+
+        # check if an intervention is active and apply it to rates
+        params <- .intervention_on_rates(
+          t = time, interventions = intervention, parameters = params
+        )
+
+        # transmission modifiers - 1.0 for baseline, user-provided for ETU risk
+        # and funeral risk
+        transmission_rate_modifiers <- c(
+          1.0, params[["etu_risk"]], params[["funeral_risk"]]
+        )
+
+        # get current transmission_rate as
+        # base rate * intervention * p(infectious)
+        # TODO: check if transmissibilities should be summed or averaged
+        current_transmission_rate <- sum(params[["transmission_rate"]] *
+          transmission_rate_modifiers *
+          sim_data[time - 1, c("infectious", "hospitalised", "funeral")]) /
+          population_size
+        exposure_prob <- 1.0 - exp(-current_transmission_rate)
+
+        # calculate new exposures
+        new_exposed <- stats::rbinom(
+          1, sim_data[time - 1, "susceptible"], exposure_prob
+        )
+        # handle non-zero new exposures
+        if (new_exposed > 0) {
+          # distribute new exposures and add past exposures moved forward by one
+          # timestep
+          exposed_current <- as.vector(
+            stats::rmultinom(1, size = new_exposed, prob = exposed_boxcar_rates)
+          ) +
+            c(exposed_past[-1], 0)
+        } else {
+          exposed_current <- c(exposed_past[-1], 0)
+        }
+
+        # handle hospitalisations first, as required for infectious compartment
+        # new hospitalisations are a proportion of individuals from infectious
+        # sub-compartments. Add a small normally distributed error to proportion
+        # hospitalised to facilitate rounding to avoid fractional individuals
+        # NOTE: proportion hospitalised = 1 - proportion community
+        hospitalised_current <- round(
+          # the SD of the normal distribution is small enough that values
+          # added to zero lead to rounding to zero
+          # first infectious_past compartment cannot be hospitalised and is
+          # transferred to funeral compartment
+          (infectious_past[-1] * (1.0 - params[["prop_community"]])) +
+            rounding_factor
+        )
+
+        # calculate new infectious individuals
+        new_infectious <- exposed_past[1]
+        # handle non-zero new infectious
+        if (new_infectious > 0) {
+          infectious_current <- as.vector(
+            stats::rmultinom(
+              1,
+              size = new_infectious, prob = infectious_boxcar_rates
+            ) +
+              c(infectious_past[-1] - hospitalised_current, 0)
+          )
+        } else {
+          infectious_current <- c(infectious_past[-1] - hospitalised_current, 0)
+        }
+
+        # continue handling hospitalisations
+        # concat zero to hospitalised_current at start as no infectious can go
+        # to this compartment
+        hospitalised_current <- c(0, hospitalised_current) +
+          c(hospitalised_past[-1], 0)
+
+        # calculate new individuals in the funeral transmission class
+        funeral_trans_current <- infectious_past[1]
+
+        # calculate new safely removed as the final hospitalised
+        # sub-compartments and new burials of potentially transmitting funerals
+        new_removed <- hospitalised_past[1] + funeral_trans_past
+
+        # set past vectors to current vectors
+        exposed_past <- exposed_current
+        infectious_past <- infectious_current
+        hospitalised_past <- hospitalised_current
+        funeral_trans_past <- funeral_trans_current
+
+        # prepare the data for output
+        sim_data[time, "susceptible"] <- sim_data[time - 1, "susceptible"] -
+          new_exposed
+        sim_data[time, "exposed"] <- sum(exposed_current)
+        sim_data[time, "infectious"] <- sum(infectious_current)
+        sim_data[time, "hospitalised"] <- sum(hospitalised_current)
+        sim_data[time, "funeral"] <- funeral_trans_current
+        sim_data[time, "removed"] <- sim_data[time - 1, "removed"] +
+          new_removed
+      }
+
+      # return simulated data matrix and time as a two element list
+      # replicate id is handled in `.output_to_df_ebola()`
+      list(x = sim_data, time = seq(0, time_end))
+    }, .options = furrr::furrr_options(
+      seed = local_seeds[1]
+    )
+  )
 
   # return output runs
   output_runs

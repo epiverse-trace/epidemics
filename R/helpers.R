f75fbf5e29f746792617a044774ac0329e01c5c6
pratikunterwegs <pratikgupte16@gmail.com>
2024-06-05 15:31:54 +0100

    Transplant #152 to new branch, WIP #127
    Co-authored-by: Banky Ahadzie <bankole.ahadzie@lshtm.ac.uk>

--- R/helpers.R
+++ R/helpers.R
@@ -359,3 +359,78 @@ new_infections <- function(data,
   # return data
   data
 }
+
+#' Get the time and size of a compartment's highest peak
+#'
+#' Get the time and size of a compartment's highest peak for all
+#' demography groups.
+#'
+#' @details
+#' This is used for epidemics with a single peak. It is useful from
+#' a public health policy point of view to determine how bad an epidemic will
+#' be and when that happens.
+#'
+#' @param data A `<data.frame>` or `<data.table>` of model output, typically
+#' the output of a compartmental model.
+#' @param compartments A character vector for the compartments of interest.
+#' @return A `<data.table>` with columns "demography_group", "compartment",
+#' "time" and "value"; these specify the name of the demography group,
+#' the epidemiological compartment, and the peak time and value for each
+#' compartment in `compartments`.
+#'
+#' @export
+#'
+#' @examples
+#' # create a population
+#' uk_population <- population(
+#'   name = "UK population",
+#'   contact_matrix = matrix(1),
+#'   demography_vector = 67e6,
+#'   initial_conditions = matrix(
+#'     c(0.9999, 0.0001, 0, 0, 0),
+#'     nrow = 1, ncol = 5L
+#'   )
+#' )
+#'
+#' # run epidemic simulation with no vaccination or intervention
+#' data <- model_default(
+#'   population = uk_population,
+#'   time_end = 600
+#' )
+#'
+#' # get the timing and peak of the exposed and infectious compartment
+#' epidemic_peak(data, c("exposed", "infectious"))
+epidemic_peak <- function(data, compartments = "infectious") {
+  # solves "no visible binding for global variable"
+  compartment <- NULL
+  value <- NULL
+  time <- NULL
+
+  # basic input checks
+  checkmate::assert_data_frame(
+    data,
+    min.cols = 4L, min.rows = 1, any.missing = FALSE
+  )
+  checkmate::assert_names(
+    colnames(data),
+    must.include = c("time", "demography_group", "compartment", "value")
+  )
+  checkmate::assert_character(
+    compartments,
+    min.len = 1,
+    any.missing = FALSE, unique = TRUE,
+    null.ok = FALSE
+  )
+
+  data.table::setDT(data)
+  out <- data[compartment %in% compartments,
+    list(
+      time = data.table::first(time[value == max(value)]),
+      value = data.table::first(max(value))
+    ),
+    by = c("demography_group", "compartment")
+  ]
+
+  # return a data.table
+  out
+}

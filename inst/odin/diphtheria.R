1198525f3ab95249d7caa3a6effef18943011350
alxsrobert <alexis.robert@lshtm.ac.uk>
2025-03-14 09:55:10 +0000

    Add odin versions of diphtheria and vacamole models

---
+++ inst/odin/diphtheria.R
@@ -0,0 +1,96 @@
+# Time-dependent parameters
+beta_t <- interpolate(time, beta, "linear")
+r_t <- interpolate(time, r, "linear") # report
+eta_t <- interpolate(time, eta, "linear") # hospitalisation
+sigma_t <- interpolate(time, sigma, "linear")
+tau1_t <- interpolate(time, tau1, "linear") # hospital entry
+tau2_t <- interpolate(time, tau2, "linear") # hospital exit
+gamma_t <- interpolate(time, gamma, "linear")
+pop_change_t[] <- interpolate(time, pop_change, "linear")
+
+## Compute reduction in transmission from rate_intervention
+# Contact matrix with time-dependent interventions
+# Then add across interventions - rate does not seem to be age-dependent, 
+# so could remove age dimension for rate_reduction and rate_reduction_total
+rate_reduction[,] <- if (t > rate_intervention_start[i] && t < rate_intervention_end[i]) 
+  rate_intervention_effect[i,j] else 0 # nolint: line_length_linter.
+rate_reduction_sum[] <- sum(rate_reduction[,i])
+rate_reduction_total[] <- 1 - min(rate_reduction_sum[i], 1)
+
+# Specify how transmission varies over time
+# FOI is contacts * infectious * transmission rate 
+# returns a matrix, must be converted to a vector/1D array
+# multiply FOI by  reduction in contact origin * reduction in contact destination * reduction in rate
+# NOTE: division by population size - this is typically included in the
+# contact matrix scaling in other models
+lambda_prod[, ] <- beta_t * I[j] * rate_reduction_total[i] / (sum(S[]) + sum(E[]) + sum(I[]) + sum(H[]) + sum(R[]))
+lambda[] <- sum(lambda_prod[i, ])
+
+# ODEs
+deriv(S[]) <-  -lambda[i] * S[i] + pop_change_t[i]
+deriv(E[]) <-  lambda[i] * S[i] - sigma_t * E[i]
+deriv(I[]) <-  sigma_t * E[i] - eta_t * tau1_t * r_t * I[i] - gamma_t * I[i]
+deriv(H[]) <-  tau1_t * eta_t * r_t * I[i] - tau2_t * H[i]
+deriv(R[]) <-  gamma_t * I[i] + tau2_t * H[i]
+
+# Initial conditions
+initial(S[]) <- init_S[i]
+initial(E[]) <- init_E[i]
+initial(I[]) <- init_I[i]
+initial(H[]) <- init_H[i]
+initial(R[]) <- init_R[i]
+
+# User-defined parameters
+n_age <- user()
+n_rate_intervention <- user()
+beta[] <- user()
+eta[] <- user()
+r[] <- user()
+sigma[] <- user()
+gamma[] <- user()
+tau1[] <- user()
+tau2[] <- user()
+pop_change[,] <- user()
+
+# Not currently used (can re-add for time-dependent parameters)
+dim(time) <- n_time
+dim(beta) <- n_time
+dim(eta) <- n_time
+dim(r) <- n_time
+dim(sigma) <- n_time
+dim(gamma) <- n_time
+dim(tau1) <- n_time
+dim(tau2) <- n_time
+dim(pop_change) <- c(n_time, n_age)
+dim(pop_change_t) <- n_age
+n_time <- user()
+time[] <- user()
+rate_intervention_start[] <- user()
+rate_intervention_end[] <- user()
+rate_intervention_effect[, ] <- user()
+init_S[] <- user()
+init_E[] <- user()
+init_I[] <- user()
+init_H[] <- user()
+init_R[] <- user()
+
+# Dimensions
+dim(lambda_prod) <- c(n_age, n_age)
+dim(lambda) <- n_age
+dim(S) <- n_age
+dim(E) <- n_age
+dim(I) <- n_age
+dim(H) <- n_age
+dim(R) <- n_age
+dim(rate_reduction) <- c(n_rate_intervention, n_age)
+dim(rate_reduction_sum) <- c(n_age)
+dim(rate_reduction_total) <- c(n_age)
+dim(rate_intervention_start) <- n_rate_intervention
+dim(rate_intervention_end) <- n_rate_intervention
+dim(rate_intervention_effect) <- c(n_rate_intervention, n_age)
+dim(init_S) <- n_age
+dim(init_E) <- n_age
+dim(init_I) <- n_age
+dim(init_H) <- n_age
+dim(init_R) <- n_age
+
--- src/registration.c
+++ src/registration.c
@@ -18,11 +18,24 @@
 */
 
 /* .C calls */
+extern void diphtheria_initmod_desolve(void *);
+extern void diphtheria_rhs_dde(void *);
+extern void diphtheria_rhs_desolve(void *);
 extern void seirv_model_initmod_desolve(void *);
 extern void seirv_model_rhs_dde(void *);
 extern void seirv_model_rhs_desolve(void *);
+extern void vacamole_initmod_desolve(void *);
+extern void vacamole_rhs_dde(void *);
+extern void vacamole_rhs_desolve(void *);
 
 /* .Call calls */
+extern SEXP diphtheria_contents(SEXP);
+extern SEXP diphtheria_create(SEXP);
+extern SEXP diphtheria_initial_conditions(SEXP, SEXP);
+extern SEXP diphtheria_metadata(SEXP);
+extern SEXP diphtheria_rhs_r(SEXP, SEXP, SEXP);
+extern SEXP diphtheria_set_initial(SEXP, SEXP, SEXP, SEXP);
+extern SEXP diphtheria_set_user(SEXP, SEXP);
 extern SEXP seirv_model_contents(SEXP);
 extern SEXP seirv_model_create(SEXP);
 extern SEXP seirv_model_initial_conditions(SEXP, SEXP);
@@ -30,15 +43,35 @@ extern SEXP seirv_model_metadata(SEXP);
 extern SEXP seirv_model_rhs_r(SEXP, SEXP, SEXP);
 extern SEXP seirv_model_set_initial(SEXP, SEXP, SEXP, SEXP);
 extern SEXP seirv_model_set_user(SEXP, SEXP);
+extern SEXP vacamole_contents(SEXP);
+extern SEXP vacamole_create(SEXP);
+extern SEXP vacamole_initial_conditions(SEXP, SEXP);
+extern SEXP vacamole_metadata(SEXP);
+extern SEXP vacamole_rhs_r(SEXP, SEXP, SEXP);
+extern SEXP vacamole_set_initial(SEXP, SEXP, SEXP, SEXP);
+extern SEXP vacamole_set_user(SEXP, SEXP);
 
 static const R_CMethodDef CEntries[] = {
+    {"diphtheria_initmod_desolve",  (DL_FUNC) &diphtheria_initmod_desolve,  1},
+    {"diphtheria_rhs_dde",          (DL_FUNC) &diphtheria_rhs_dde,          1},
+    {"diphtheria_rhs_desolve",      (DL_FUNC) &diphtheria_rhs_desolve,      1},
     {"seirv_model_initmod_desolve", (DL_FUNC) &seirv_model_initmod_desolve, 1},
     {"seirv_model_rhs_dde",         (DL_FUNC) &seirv_model_rhs_dde,         1},
     {"seirv_model_rhs_desolve",     (DL_FUNC) &seirv_model_rhs_desolve,     1},
+    {"vacamole_initmod_desolve",    (DL_FUNC) &vacamole_initmod_desolve,    1},
+    {"vacamole_rhs_dde",            (DL_FUNC) &vacamole_rhs_dde,            1},
+    {"vacamole_rhs_desolve",        (DL_FUNC) &vacamole_rhs_desolve,        1},
     {NULL, NULL, 0}
 };
 
 static const R_CallMethodDef CallEntries[] = {
+    {"diphtheria_contents",            (DL_FUNC) &diphtheria_contents,            1},
+    {"diphtheria_create",              (DL_FUNC) &diphtheria_create,              1},
+    {"diphtheria_initial_conditions",  (DL_FUNC) &diphtheria_initial_conditions,  2},
+    {"diphtheria_metadata",            (DL_FUNC) &diphtheria_metadata,            1},
+    {"diphtheria_rhs_r",               (DL_FUNC) &diphtheria_rhs_r,               3},
+    {"diphtheria_set_initial",         (DL_FUNC) &diphtheria_set_initial,         4},
+    {"diphtheria_set_user",            (DL_FUNC) &diphtheria_set_user,            2},
     {"seirv_model_contents",           (DL_FUNC) &seirv_model_contents,           1},
     {"seirv_model_create",             (DL_FUNC) &seirv_model_create,             1},
     {"seirv_model_initial_conditions", (DL_FUNC) &seirv_model_initial_conditions, 2},
@@ -46,6 +79,13 @@ static const R_CallMethodDef CallEntries[] = {
     {"seirv_model_rhs_r",              (DL_FUNC) &seirv_model_rhs_r,              3},
     {"seirv_model_set_initial",        (DL_FUNC) &seirv_model_set_initial,        4},
     {"seirv_model_set_user",           (DL_FUNC) &seirv_model_set_user,           2},
+    {"vacamole_contents",              (DL_FUNC) &vacamole_contents,              1},
+    {"vacamole_create",                (DL_FUNC) &vacamole_create,                1},
+    {"vacamole_initial_conditions",    (DL_FUNC) &vacamole_initial_conditions,    2},
+    {"vacamole_metadata",              (DL_FUNC) &vacamole_metadata,              1},
+    {"vacamole_rhs_r",                 (DL_FUNC) &vacamole_rhs_r,                 3},
+    {"vacamole_set_initial",           (DL_FUNC) &vacamole_set_initial,           4},
+    {"vacamole_set_user",              (DL_FUNC) &vacamole_set_user,              2},
     {NULL, NULL, 0}
 };
 

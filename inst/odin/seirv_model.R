fa4952b1aa53eb69535329e7f09642be74f6b63c
bahadzie <bahadzie@gmail.com>
2025-04-02 14:51:34 +0000

    chore: fix lintr errors in odin model files

--- inst/odin/seirv_model.R
+++ inst/odin/seirv_model.R
@@ -6,39 +6,62 @@ gamma_t <- interpolate(time, gamma, "linear")
 ## Compute reduction in transmission from contacts_intervention
 # Contact matrix with time-dependent interventions
 # Then add across interventions - need to check this
-contact_reduction[, ] <- if (t > contact_intervention_start[i] && t < contact_intervention_end[i]) 
-  contact_intervention_effect[i, j] else 0 # nolint: line_length_linter.
+contact_reduction[, ] <-
+  if (t > contact_intervention_start[i] && t < contact_intervention_end[i]) {
+    contact_intervention_effect[i, j]
+  } else {
+    0
+  } # nolint: line_length_linter.
 contact_reduction_sum[] <- sum(contact_reduction[, i])
-contact_reduction_total[] <- 1 - min(contact_reduction_sum[i],1)
+contact_reduction_total[] <- 1 - min(contact_reduction_sum[i], 1)
 
 
 ## Compute reduction in transmission from rate_intervention
 # Contact matrix with time-dependent interventions
-# Then add across interventions - rate does not seem to be age-dependent, 
+# Then add across interventions - rate does not seem to be age-dependent,
 # so could remove age dimension for rate_reduction and rate_reduction_total
-rate_reduction[,] <- if (t > rate_intervention_start[i] && t < rate_intervention_end[i]) 
-  rate_intervention_effect[i,j] else 0 # nolint: line_length_linter.
-rate_reduction_sum[] <- sum(rate_reduction[,i])
+rate_reduction[, ] <-
+  if (t > rate_intervention_start[i] && t < rate_intervention_end[i]) {
+    rate_intervention_effect[i, j]
+  } else {
+    0
+  } # nolint: line_length_linter.
+rate_reduction_sum[] <- sum(rate_reduction[, i])
 rate_reduction_total[] <- 1 - min(rate_reduction_sum[i], 1)
 
 # Specify how transmission varies over time
-# FOI is contacts * infectious * transmission rate 
+# FOI is contacts * infectious * transmission rate
 # returns a matrix, must be converted to a vector/1D array
-# multiply FOI by  reduction in contact origin * reduction in contact destination * reduction in rate
-lambda_prod[, ] <- C[i, j] * I[j] * beta_t * contact_reduction_total[j] * contact_reduction_total[i] * 
-  rate_reduction_total[i]
+# multiply FOI by  reduction in contact origin * reduction in contact destination * reduction in rate # nolint: line_length_linter.
+lambda_prod[, ] <- C[i, j] * I[j] * beta_t * contact_reduction_total[j] *
+  contact_reduction_total[i] * rate_reduction_total[i]
 lambda[] <- sum(lambda_prod[i, ])
 
 # Vaccination - indexing over age groups
-vax_rate[] <- if(S[i] == 0) 0 else if (t >= vax_start[i] && t <= vax_end[i]) vax_nu[i]/S[i] else 0
+vax_rate[] <- if (S[i] == 0) {
+  0
+} else if (t >= vax_start[i] && t <= vax_end[i]) {
+  vax_nu[i] / S[i]
+} else {
+  0
+}
 
 # ODEs
-deriv(S[]) <- if(vax_rate[i] + lambda[i] <= 1) -(lambda[i] + vax_rate[i]) * S[i] else - S[i]
+deriv(S[]) <- if (vax_rate[i] + lambda[i] <= 1) {
+  -(lambda[i] + vax_rate[i]) * S[i]
+} else {
+  -S[i]
+}
 deriv(E[]) <- lambda[i] * S[i] - sigma_t * E[i]
 deriv(I[]) <- sigma_t * E[i] - gamma_t * I[i]
 deriv(R[]) <- gamma_t * I[i]
-deriv(V[]) <- if(vax_rate[i] + lambda[i] <= 1) vax_rate[i] * S[i] else 
-  if(vax_rate[i] > 0) S[i]*(1-lambda[i]) else 0
+deriv(V[]) <- if (vax_rate[i] + lambda[i] <= 1) {
+  vax_rate[i] * S[i]
+} else if (vax_rate[i] > 0) {
+  S[i] * (1 - lambda[i])
+} else {
+  0
+}
 
 # Initial conditions
 initial(S[]) <- init_S[i]

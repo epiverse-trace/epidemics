25dfab26144af03f4d62c221f1a19cd6325a1645
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2026-07-02 14:37:13 +0100

    apply demography group labelling fix to model_diphtheria and model_vacamole, relates #278

--- R/model_diphtheria.R
+++ R/model_diphtheria.R
@@ -382,8 +382,12 @@ model_diphtheria <- function(population,
     temp <- value <- temp_compartment <- temp_demography <-
       compartment <- demography_group <- `:=` <- time <- NULL
 
+    # width of the zero-padded numeric prefix used to label and sort demography
+    # groups; must accommodate the largest group index (e.g. 10-12 need 2 chars)
+    n_demography_prefix <- nchar(as.character(n_age))
+
     age_group_mappings <- paste0( # properly label demography groups
-      seq_len(n_age),
+      formatC(seq_len(n_age), width = n_demography_prefix, flag = "0"),
       c(
         rownames(population[[1]]$contact_matrix),
         names(population[[1]]$demography_vector),
@@ -409,7 +413,8 @@ model_diphtheria <- function(population,
       , list(
         time = t, # alternative to using data.table::setnames(dt, "t", "time")
         temp_compartment = substring(temp, 1L, 1L), # e.g. S[1] -> S
-        temp_demography = substring(temp, 3L, 3L), # e.g. S[1] -> 1
+        # e.g. S[10] -> 10
+        temp_demography = substring(temp, 3L, nchar(as.character(temp)) - 1L),
         value
       )
     ][ # |> the DT way (piping the data.table way)
@@ -426,7 +431,10 @@ model_diphtheria <- function(population,
       ,
       `:=`( # used as the prefix form to update multiple columns
         # remove prepended numbers from `mapping`
-        demography_group = substring(demography_group, 2L), # e.g. [0,20), ...
+        # e.g. [0,20), ...
+        demography_group = substring(
+          demography_group, n_demography_prefix + 1L
+        ),
         compartment = substring(compartment, 2L) # e.g. susceptible, exposed,
       )
     ][ # |> the DT way

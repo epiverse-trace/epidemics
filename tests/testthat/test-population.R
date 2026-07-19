7fa4aa0a1103b4989daaafe7f6cc46e34f573780
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2026-07-19 17:19:43 +0100

    replace socialmixr::survey_country_population with {wpp2024} data wrangling in tests, relates #281

--- tests/testthat/test-population.R
+++ tests/testthat/test-population.R
@@ -1,13 +1,18 @@
 # Basic tests to check for functionality of population class
 # Prepare contact matrix and demography vector
 polymod <- socialmixr::polymod
+
+# demography data from the wpp2024 package
+data("popAge1dt", package = "wpp2024")
+uk_pop <- popAge1dt |>
+  dplyr::filter(name == "United Kingdom", year == 2006) |>
+  dplyr::select(lower.age.limit = age, population = pop) |>
+  dplyr::mutate(population = population * 1000)
+
 contact_data <- socialmixr::contact_matrix(
   polymod,
   countries = "United Kingdom",
-  survey_pop = socialmixr::survey_country_population(
-    polymod,
-    countries = "United Kingdom"
-  ),
+  survey_pop = uk_pop,
   age_limits = c(0, 40, 65),
   symmetric = TRUE,
   return_demography = TRUE
